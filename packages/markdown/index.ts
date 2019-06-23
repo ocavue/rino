4730e80eae045839a587efefcf6a73da8e3c5fa5
ocavue <ocavue@gmail.com>
2019-06-23 19:28:44 +0800

    chore: Use parcel's experimental tree shaking feature

--- packages/markdown/index.ts
+++ packages/markdown/index.ts
@@ -1,7 +1,10 @@
 // Defines a parser and serializer for [CommonMark](http://commonmark.org/) text.
 
 export { schema } from "./src/schema"
-export { Token } from './src/token'
 export { InlineLexer }from './src/lexer'
 export { defaultMarkdownParser, MarkdownParser } from "./src/markdown2prosemirror"
 export { MarkdownSerializer, defaultMarkdownSerializer, MarkdownSerializerState } from "./src/prosemirror2markdown"
+
+// https://github.com/parcel-bundler/parcel/issues/3153
+import { Token } from './src/token'
+export type Token = Token
