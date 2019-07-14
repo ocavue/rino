eee210b38a9e18e1db92ad754109e1ff6998c0f4
ocavue <ocavue@users.noreply.github.com>
2019-07-14 22:03:05 +0800

    feat: Update sidebar style (#16)

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-    <div>
-        <input type="button" value="Create note" @click="createNote" />
+    <div id="app">
         <Sidebar></Sidebar>
-        <router-view></router-view>
+        <router-view id="main"></router-view>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue"
 
-import { Note, firebase } from "./controller"
+import { firebase } from "./controller"
 
 import Sidebar from "./components/Sidebar.vue"
 
@@ -47,30 +46,24 @@ export default Vue.extend({
                 .signInAnonymously()
                 .catch(error => console.error(`Failed to sign in: ${error}`))
         },
-        createNote: async function() {
-            let doc = await Note.create()
-            await Note.update(doc.id, "# default content")
-            this.$router.push({ path: `/e/${doc.id}` })
-        },
     },
 })
 </script>
 
-<style lang="sass">
-body
-    font-family: "Fira Mono", "DejaVu Sans Mono", "Menlo", "Consolas", "Liberation Mono", "Monaco", "Lucida Console", monospace
-    padding: 0
-    margin: 0
+<style lang="scss">
+body {
+    font-family: "Fira Mono", "DejaVu Sans Mono", "Menlo", "Consolas", "Liberation Mono", "Monaco",
+        "Lucida Console", monospace;
+    padding: 0;
+    margin: 0;
+}
 
-header
-    display: flex
-    flex-flow: wrap
-    justify-content: space-between
-    align-items: center
-    height: 3em
-    line-height: 1em
-    padding-left: 16px
-    padding-right: 16px
-    background-color: darkmagenta
-    color: rgba(255, 255, 255, 0.8)
+#app {
+    display: flex;
+    min-height: 100vh;
+}
+
+#main {
+    flex: 1;
+}
 </style>
