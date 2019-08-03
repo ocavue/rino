08ac76620683d9f2768d539858bb0ebee05e7ad1
ocavue <ocavue@gmail.com>
2019-08-04 02:11:43 +0800

    test: Avoid infinite waitting

--- scripts/wait-server.sh
+++ scripts/wait-server.sh
@@ -3,7 +3,14 @@
 # Wait until the server starts to respond.
 
 url=$1
-until $(curl --output /dev/null --silent --head --fail $url); do
-    echo "waitting $url"
-    sleep 1s
+
+for times in {1..120}
+do
+    if [[ -f "$(curl --output /dev/null --silent --head --fail $url)" ]]
+    then
+        break
+    else
+        echo "waitting $url #$times"
+        sleep 1s
+    fi
 done
