8a56c61cfc600e7bdd7e9a9e464758272b2f0455
ocavue <ocavue@gmail.com>
2019-08-04 17:18:11 +0800

    review

--- packages/app/src/controller/auth.ts
+++ packages/app/src/controller/auth.ts
@@ -1,8 +1,10 @@
 import { firebase } from "./firebase"
 import { generateRandomId } from "../utils"
+import { env } from "./config"
 
 export async function sendSignInLink(email: string) {
-    const url = `http://localhost:1234/finish-sign-up/${generateRandomId()}`
+    const host = env.TESTING ? "http://localhost:1234" : "https://rino.app"
+    const url = `${host}/finish-sign-up/${generateRandomId()}`
     return await firebase.auth().sendSignInLinkToEmail(email, { url, handleCodeInApp: true })
 }
 
