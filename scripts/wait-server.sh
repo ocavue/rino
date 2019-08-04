def3e86257a45ac56c3c50f53200c1afa02b5613
ocavue <ocavue@gmail.com>
2019-08-04 15:03:13 +0800

    chore: Fix wait-server.sh

--- scripts/wait-server.sh
+++ scripts/wait-server.sh
@@ -6,11 +6,7 @@ url=$1
 
 for times in {1..120}
 do
-    if [[ -f "$(curl --output /dev/null --silent --head --fail $url)" ]]
-    then
-        break
-    else
-        echo "waitting $url #$times"
-        sleep 1s
-    fi
+    curl --output /dev/null --silent --head --fail $url \
+    && break \
+    || echo "waitting $url #$times" && sleep 1s
 done
