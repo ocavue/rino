ab70da291e7eb66bd49bba669d8c1b2a3bb471e5
ocavue <ocavue@gmail.com>
2019-10-05 07:36:52 +0800

    style

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,27 @@
 <template>
-    <div id="login">
-        <p v-if="!sented">
-            <label>
-                Sign in with email:
-                <input v-model="email" placeholder="Email" name="email" type="email" />
-            </label>
-            <button @click="login">Next</button>
-        </p>
-        <p v-else class="login__message" data-testid="login-message">
-            A sign-in email with additional instructions was sent to <strong>{{ email }}</strong
-            >. Check your email to complete sign-in.
-        </p>
-    </div>
+    <v-app>
+        <v-content id="login">
+            <v-container fluid>
+                <v-card v-if="!sented">
+                    <v-card-title>Sign in with email</v-card-title>
+                    <v-card-text>
+                        <v-text-field v-model="email" label="Email"></v-text-field>
+                        <p v-if="error" class="login__error">{{ error }}</p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn :loading="senting" color="primary" @click="login">Next</v-btn>
+                    </v-card-actions>
+                </v-card>
+                <v-card v-else>
+                    <v-card-text class="login__message" data-testid="login-message">
+                        A sign-in email with additional instructions was sent to
+                        <strong>{{ email }}</strong
+                        >. Check your email to complete sign-in.
+                    </v-card-text>
+                </v-card>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script lang="ts">
@@ -23,6 +33,8 @@ export default Vue.extend({
     name: "Login",
     data: () => ({
         email: "",
+        error: "",
+        senting: false,
         sented: false,
     }),
     mounted: function() {
@@ -38,16 +50,29 @@ export default Vue.extend({
     },
     methods: {
         login: function() {
+            this.error = ""
+            this.senting = true
             sendSignInLink(this.email)
                 .then(() => {
+                    this.senting = false
                     this.sented = true
                     // The link was successfully sent. Inform the user.
                     // Save the email locally so you don't need to ask the user for it again
                     // if they open the link on the same device.
                     window.localStorage.setItem("emailForSignIn", this.email)
                 })
-                .catch(error => console.error(error))
+                .catch(error => {
+                    this.senting = false
+                    this.error = error
+                    console.error(error)
+                })
         },
     },
 })
 </script>
+
+<style lang="scss" scoped>
+.login__error {
+    color: red;
+}
+</style>
