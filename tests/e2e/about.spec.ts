fa9b07a1b33030f440b936c9cf5140be7fb664fa
ocavue <ocavue@users.noreply.github.com>
2019-10-09 20:14:16 +0800

    feat: Add the "about" dialog (#40)

---
+++ tests/e2e/about.spec.ts
@@ -0,0 +1,30 @@
+import { goto, wait, click, getInnerText, sleep } from "./utils"
+
+describe("About", function() {
+    test("Open the dialog", async () => {
+        await goto("/")
+        await click("appbar-btn-dots")
+        await sleep(200)
+        await click("appbar-menu-item-about")
+        await wait("about-dialog")
+    })
+
+    test("Check version format", async () => {
+        // Should be something like 'Version 0.23.4 (2fd4e1c)'
+        const received = await getInnerText("about-dialog-version")
+        const expected = expect.stringMatching(/^Version \d+\.\d+.\d+ \([0-9a-z]{7}\)$/)
+        expect(received).toEqual(expected)
+    })
+
+    test("Check copyright format", async () => {
+        const year = new Date().getFullYear()
+        const received = await getInnerText("about-dialog-copyright")
+        const expected = `Copyright © ${year} Ocavue. All rights reserved.`
+        expect(received).toEqual(expected)
+    })
+
+    test("Close the dialog", async () => {
+        await page.mouse.click(5, 5)
+        await wait("about-dialog", { hidden: true })
+    })
+})
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+const readFileSync = require("fs").readFileSync
+const execSync = require("child_process").execSync
+
+process.env.VUE_APP_VERSION = JSON.parse(readFileSync("./package.json")).version
+process.env.VUE_APP_COMMIT = `${execSync("git rev-parse --short HEAD")}`.trim()
+
 module.exports = {
     lintOnSave: false,
     pwa: {
