1259b97f4a8a54e8c8fd1b68b194b8c2d88cde00
ocavue <ocavue@gmail.com>
2019-11-06 21:08:05 +0800

    test: Fix incorrect test coverage for .vue files

--- tests/setup.js
+++ tests/setup.js
@@ -1,15 +1,35 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function debug() {
+    console.log(
+        await page.evaluate(() => {
+            let result = {}
+            for (let [file, coverage] of Object.entries(window.__coverage__ || {})) {
+                result[file] = {
+                    path: coverage.path,
+                    inputSourceMap: {
+                        file: coverage.inputSourceMap.file,
+                        sources: coverage.inputSourceMap.sources,
+                    },
+                }
+            }
+            return JSON.stringify(result, null, 2)
+        }),
+    )
+}
+
 async function adjustVueCoverage() {
     // vuetify-loader will make coverage[<Vue-SFC-path>].inputSourceMap.sources[0] like
     // "vuetify-loader/lib/loader.js!cache-loader/dist/cjs.js??ref--0-0!vue-loader/lib/index.js??vue-loader-options!src/App.vue?vue&type=script&lang=ts&".
     // This function adjust it into a normal file path, so that `jest-puppeteer-istanbul` can handler it correctly.
+
+    // await debug()
     await page.evaluate(() => {
         for (let [file, coverage] of Object.entries(window.__coverage__ || {})) {
             coverage.path = file
             coverage.inputSourceMap.file = file
-            coverage.inputSourceMap.sources = file
+            coverage.inputSourceMap.sources = [file]
         }
     })
-    // console.log(await page.evaluate(() => window.__coverage__))
 }
 
 afterEach(async () => {
