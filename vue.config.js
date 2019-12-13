dd5d7f626d27b5fe70590d741e47bfcb6db4475a
ocavue <ocavue@gmail.com>
2019-12-13 13:09:17 +0800

    fix: Fix PWA catch

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/camelcase */
+
 const readFileSync = require("fs").readFileSync
 const execSync = require("child_process").execSync
 
@@ -7,9 +9,13 @@ process.env.VUE_APP_COMMIT = `${execSync("git rev-parse --short HEAD")}`.trim()
 module.exports = {
     lintOnSave: false,
     pwa: {
+        // Documents for vue-cli pwa plugin: https://www.npmjs.com/package/@vue/cli-plugin-pwa
         name: "Rino",
         themeColor: "#ededed",
-        assetsVersion: "1",
+        display: "standalone",
+        manifestOptions: {
+            background_color: "#000000",
+        },
     },
     css: {
         // Ignore mini-css-extract-plugin's "Conflicting order between:" error.
