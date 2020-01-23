c76bb4c5d6bdde1b4eadd73c6387f07e4b852825
ocavue <ocavue@gmail.com>
2020-01-23 20:26:00 +0800

    feat: Beautify 404 page

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,8 +1,28 @@
 import { NextPageContext } from "next"
+import Alert from "src/views/Alert"
 import React from "react"
 
-function Error({ statusCode }: { statusCode: number }) {
-    return <div data-testid={statusCode}>{statusCode}</div>
+// Some common status codes
+const statusCodeMap: Record<string, string> = {
+    "400": "Bad Request",
+    "401": "Unauthorized",
+    "403": "Forbidden",
+    "404": "This page could not be found.",
+    "405": "Method Not Allowed",
+    "408": "Request Timeout",
+    "421": "Misdirected Request",
+    "429": "Too Many Requests",
+    "500": "Internal Server Error",
+    "502": "Bad Gateway",
+    "503": "Service Unavailable",
+    "504": "Gateway Timeout",
+}
+
+function Error(props: { statusCode?: number }) {
+    // In static build, `statusCode` is empty since `getInitialProps` will not be executed.
+    const code = String(props.statusCode || 404)
+    const message = statusCodeMap[code] || `The server return status code "${code}".`
+    return <Alert title={code} message={message} />
 }
 
 Error.getInitialProps = ({ res, err }: NextPageContext) => {
--- tests/e2e/1.not-found.spec.ts
+++ tests/e2e/1.not-found.spec.ts
@@ -2,5 +2,5 @@ import { goto, wait } from "./utils"
 
 test("404 page", async () => {
     await goto("/this-page-does-not-exist")
-    await wait("404")
+    await wait("alert-404")
 })
