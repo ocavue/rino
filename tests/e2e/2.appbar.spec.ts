2177d09376ae592cb54bd2ad84084a854e72fa35
ocavue <ocavue@gmail.com>
2020-01-29 05:38:20 +0800

    fix

--- tests/e2e/2.appbar.spec.ts
+++ tests/e2e/2.appbar.spec.ts
@@ -1,22 +1,40 @@
-import { cleanNotes, createNote, deleteNote, login } from "./actions"
+import { cleanNotes, createNote, deleteNote, login, signOut } from "./actions"
 import { click, getAll, sleep, wait, waitAnimation } from "./utils"
 
-describe("Delete notes", function() {
-    async function expectNoteNumber(expected: number) {
-        await sleep(500)
-        const notes = await getAll("sidebar-notes-list-item")
-        expect(notes).toHaveLength(expected)
-    }
+async function getNoteNumber() {
+    await sleep(500)
+    const notes = await getAll("sidebar-notes-list-item")
+    return notes.length
+}
 
+async function expectNoteNumber(expected: number) {
+    expect(await getNoteNumber()).toEqual(expected)
+}
+
+describe("Delete user notes", function() {
     test("Parapre enviorment", async () => {
         await login()
         await cleanNotes()
         await sleep(1000)
-        await page.reload()
     })
     test("Parpare notes", async () => {
         await waitAnimation(createNote())
         await waitAnimation(createNote())
+    })
+    test("Delete note A", async () => {
+        await wait("sidebar-notes-list-item")
+        const notes = await getNoteNumber()
+        expect(notes).toBeGreaterThanOrEqual(1)
+
+        await click("sidebar-notes-list-item")
+        await waitAnimation(deleteNote(), 1000)
+        await expectNoteNumber(notes - 1)
+    })
+})
+
+describe("Delete premade notes", function() {
+    test("Parapre enviorment", async () => {
+        await signOut()
     })
     test("Verify initial note number", async () => {
         await wait("sidebar-notes-list-item")
@@ -27,10 +45,5 @@ describe("Delete notes", function() {
         await click("sidebar-notes-list-item")
         await waitAnimation(deleteNote(), 1000)
         await expectNoteNumber(1)
-    })
-    test("Delete note B", async () => {
-        await click("sidebar-notes-list-item")
-        await waitAnimation(deleteNote(), 1000)
-        await expectNoteNumber(0)
     })
 })
