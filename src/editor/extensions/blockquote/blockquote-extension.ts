b5c004385ecd99a81cdf96426a964ffdbeb15ca7
ocavue <ocavue@gmail.com>
2020-03-17 04:56:34 +0800

    move

--- src/editor/extensions/blockquote/blockquote-extension.ts
+++ src/editor/extensions/blockquote/blockquote-extension.ts
@@ -1,7 +1,7 @@
 import { BlockquoteExtension } from "@remirror/core-extensions"
-import { MarkdownNodeExtension } from "../utils"
-import { NodeSerializerOptions } from "../transform/serializer"
-import { ParserTokenType } from "../transform/parser-type"
+import { MarkdownNodeExtension } from "src/editor/utils"
+import { NodeSerializerOptions } from "src/editor/transform/serializer"
+import { ParserTokenType } from "src/editor/transform/parser-type"
 
 export class RinoBlockquoteExtension extends BlockquoteExtension implements MarkdownNodeExtension {
     public styles() {
--- src/editor/extensions/heading/heading-extension.ts
+++ src/editor/extensions/heading/heading-extension.ts
@@ -5,9 +5,9 @@ import {
     findParentNodeOfType,
 } from "@remirror/core"
 import { HeadingExtension, HeadingExtensionOptions } from "@remirror/core-extensions"
-import { MarkdownNodeExtension } from "../utils"
-import { NodeSerializerOptions } from "../transform/serializer"
-import { ParserTokenType } from "../transform/parser-type"
+import { MarkdownNodeExtension } from "src/editor/utils"
+import { NodeSerializerOptions } from "src/editor/transform/serializer"
+import { ParserTokenType } from "src/editor/transform/parser-type"
 import { setBlockType } from "prosemirror-commands"
 import Token from "markdown-it/lib/token"
 
