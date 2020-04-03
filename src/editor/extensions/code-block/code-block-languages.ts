1c4296be95774a6a7110ce6bf7cdc6fd5af2c996
ocavue <ocavue@gmail.com>
2020-04-03 15:28:32 +0800

    feat: add json support

---
+++ src/editor/extensions/code-block/code-block-languages.ts
@@ -0,0 +1,21 @@
+import clike from "refractor/lang/clike"
+import css from "refractor/lang/css"
+import go from "refractor/lang/go"
+import java from "refractor/lang/java"
+import javascript from "refractor/lang/javascript"
+import json from "refractor/lang/json"
+import markup from "refractor/lang/markup"
+import python from "refractor/lang/python"
+import typescript from "refractor/lang/typescript"
+
+export const supportedLanguages = [
+    clike,
+    css,
+    go,
+    java,
+    javascript,
+    json,
+    markup,
+    python,
+    typescript,
+]
