cdd00ba64b3878174443854c7b38bc68f055a896
ocavue <ocavue@gmail.com>
2020-04-19 15:44:16 +0800

    test: add test for dense snackbar

--- tests/e2e/1.sign-in-snackbar.spec.ts
+++ tests/e2e/1.sign-in-snackbar.spec.ts
@@ -1,4 +1,4 @@
-import { signInSnackbarDelay } from "src/constants"
+import { SIGN_IN_SNACKBAR_HIDE_DELAY, SIGN_IN_SNACKBAR_SHOW_DELAY } from "src/constants"
 
 import { login, signOut } from "./actions"
 import { click, sleep, wait } from "./utils"
@@ -6,16 +6,26 @@ import { click, sleep, wait } from "./utils"
 describe("Sign up snackbar", function () {
     test("Hide snackbar", async () => {
         await login()
-        await sleep(signInSnackbarDelay + 1000)
-        await wait("sign-in-snack-bar", { hidden: true, visible: false })
+        await sleep(SIGN_IN_SNACKBAR_SHOW_DELAY + 1000)
+        await wait("full-sign-in-snack-bar", { hidden: true, visible: false })
+        await wait("dense-sign-in-snack-bar", { hidden: true, visible: false })
     })
-    test("Show snackbar", async () => {
+    test("Show full snackbar", async () => {
         await signOut()
-        await sleep(signInSnackbarDelay)
-        await wait("sign-in-snack-bar")
+        await sleep(SIGN_IN_SNACKBAR_SHOW_DELAY)
+        await wait("full-sign-in-snack-bar")
     })
-    test("Click snackbar button", async () => {
-        await click("sign-in-snack-bar-button")
+    test("Show dense snackbar", async () => {
+        await sleep(SIGN_IN_SNACKBAR_HIDE_DELAY + 1000)
+        await wait("dense-sign-in-snack-bar")
+        await wait("full-sign-in-snack-bar", { hidden: true, visible: false })
+    })
+    test("Click dense snackbar button", async () => {
+        await click("dense-sign-in-snack-bar-button")
+        await wait("full-sign-in-snack-bar")
+    })
+    test("Click full snackbar button", async () => {
+        await click("full-sign-in-snack-bar-button")
         await wait("login-form-card")
     })
 })
