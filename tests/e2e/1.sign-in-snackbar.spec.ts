05a8063fda08e8704b685dbcd01a8785477d1f1d
ocavue <ocavue@gmail.com>
2020-04-19 16:11:06 +0800

    wip: debug test

--- tests/e2e/1.sign-in-snackbar.spec.ts
+++ tests/e2e/1.sign-in-snackbar.spec.ts
@@ -4,28 +4,30 @@ import { login, signOut } from "./actions"
 import { click, sleep, wait } from "./utils"
 
 describe("Sign up snackbar", function () {
+    jest.setTimeout(180000)
+
     test("Hide snackbar", async () => {
         await login()
         await sleep(SIGN_IN_SNACKBAR_SHOW_DELAY + 1000)
         await wait("full-sign-in-snack-bar", { hidden: true, visible: false })
         await wait("dense-sign-in-snack-bar", { hidden: true, visible: false })
     })
-    test("Show full snackbar", async () => {
-        await signOut()
-        await sleep(SIGN_IN_SNACKBAR_SHOW_DELAY)
-        await wait("full-sign-in-snack-bar")
-    })
-    test("Show dense snackbar", async () => {
-        await sleep(SIGN_IN_SNACKBAR_HIDE_DELAY + 1000)
-        await wait("dense-sign-in-snack-bar")
-        await wait("full-sign-in-snack-bar", { hidden: true, visible: false })
-    })
-    test("Click dense snackbar button", async () => {
-        await click("dense-sign-in-snack-bar-button")
-        await wait("full-sign-in-snack-bar")
-    })
-    test("Click full snackbar button", async () => {
-        await click("full-sign-in-snack-bar-button")
-        await wait("login-form-card")
-    })
+    // test("Show full snackbar", async () => {
+    //     await signOut()
+    //     await sleep(SIGN_IN_SNACKBAR_SHOW_DELAY)
+    //     await wait("full-sign-in-snack-bar")
+    // })
+    // test("Show dense snackbar", async () => {
+    //     await sleep(SIGN_IN_SNACKBAR_HIDE_DELAY + 1000)
+    //     await wait("dense-sign-in-snack-bar")
+    //     await wait("full-sign-in-snack-bar", { hidden: true, visible: false })
+    // })
+    // test("Click dense snackbar button", async () => {
+    //     await click("dense-sign-in-snack-bar-button")
+    //     await wait("full-sign-in-snack-bar")
+    // })
+    // test("Click full snackbar button", async () => {
+    //     await click("full-sign-in-snack-bar-button")
+    //     await wait("login-form-card")
+    // })
 })
