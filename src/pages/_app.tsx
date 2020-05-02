21014a5a7a7c0f5d3326a7930e939e169a81b5d3
ocavue <ocavue@gmail.com>
2020-05-02 16:52:11 +0800

    f

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 // Import Global CSS
 // https://github.com/zeit/next.js/blob/v9.2.0/errors/css-global.md
-import "../../node_modules/github-markdown-css/github-markdown.css"
 import "../../node_modules/prosemirror-tables/style/tables.css"
 import "../../node_modules/prosemirror-view/style/prosemirror.css"
 import "../../src/editor/components/markdown.css"
 import "../../src/style/develop.css"
+import "../../src/editor/theme/github.css"
 
 import Head from "next/head"
 import React from "react"
