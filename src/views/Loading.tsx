a2d78eba0d57e226a9ae47a3ab53263b8c7bca25
ocavue <ocavue@gmail.com>
2020-06-01 04:03:02 +0800

    feat: update loading style

--- src/views/Loading.tsx
+++ src/views/Loading.tsx
@@ -6,7 +6,14 @@ export function Loading() {
     return (
         <Center>
             {/* This component will be rendered before the css so we specify the font family inline here.*/}
-            <p style={{ fontFamily: "sans-serif" }}>Loading</p>
+            <p
+                style={{
+                    fontFamily: "SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace",
+                    fontSize: "18px",
+                }}
+            >
+                Loading
+            </p>
         </Center>
     )
 }
