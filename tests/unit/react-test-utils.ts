55269faf2ba2d16333182579dbcb670072d56641
ocavue <ocavue@users.noreply.github.com>
2020-06-13 01:15:43 +0800

    test: add more unit test cases for UI components (#201)

--- tests/unit/react-test-utils.ts
+++ tests/unit/react-test-utils.ts
@@ -2,3 +2,18 @@ export const TestHook = <T>({ callback }: { callback: () => T }) => {
     callback()
     return null
 }
+
+/**
+ * use this function to mock "next/link"
+ * https://github.com/vercel/next.js/issues/4012
+ *
+ * example:
+ *
+ * ```
+ * jest.mock("next/link", () => mockNextLink)
+ * ```
+ *
+ */
+export function mockNextLink({ children }: any) {
+    return children
+}
