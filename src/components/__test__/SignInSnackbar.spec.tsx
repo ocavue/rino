935ba666e3a4eb8e64e03a2d652035fb86fd0ab6
ocavue <ocavue@gmail.com>
2020-06-13 00:57:01 +0800

    add tests

---
+++ src/components/__test__/SignInSnackbar.spec.tsx
@@ -0,0 +1,69 @@
+import { ThemeProvider } from "@material-ui/core"
+import { fireEvent, render, screen, waitFor, waitForDomChange } from "@testing-library/react"
+import { act } from "@testing-library/react"
+// import NextLink from "next/link"
+import React from "react"
+
+import { SIGN_IN_SNACKBAR_SHOW_DELAY } from "src/constants"
+import { EditContainer } from "src/controller"
+import { StoreContainer } from "src/store"
+import { mockNextLink, TestHook } from "tests/unit/react-test-utils"
+
+import { SignInSnackbar } from "../SignInSnackbar"
+
+jest.useFakeTimers()
+jest.mock("next/link", () => mockNextLink)
+
+function renderWithCallback(component: React.ReactNode, callback: () => any) {
+    return render(
+        <div>
+            <StoreContainer.Provider>
+                <EditContainer.Provider>
+                    {component}
+                    <TestHook callback={callback} />
+                </EditContainer.Provider>
+            </StoreContainer.Provider>
+            ,
+        </div>,
+    )
+}
+
+describe("<SignInSnackbar />", () => {
+    test("without login user", () => {
+        // Parpare the environment
+        let storeHooks = {} as ReturnType<typeof StoreContainer.useContainer>
+        renderWithCallback(<SignInSnackbar />, () => {
+            storeHooks = StoreContainer.useContainer()
+        })
+        act(() => {
+            storeHooks.state.setLoadingData(false)
+            storeHooks.state.setLoadingUser(false)
+            storeHooks.auth.setUser(null)
+
+            jest.advanceTimersByTime(SIGN_IN_SNACKBAR_SHOW_DELAY + 1000)
+        })
+
+        // Show the "full" bar at the begin
+        expect(screen.queryByTestId("full-sign-in-snack-bar")).not.toBeNull()
+        expect(screen.queryByTestId("dense-sign-in-snack-bar")).toBeNull()
+
+        // Show the "dense" bar after clicking the close button
+        fireEvent.click(screen.getByTestId("full-sign-in-snack-bar-close-button"))
+        act(() => {
+            jest.runAllTimers()
+        })
+        expect(screen.queryByTestId("full-sign-in-snack-bar")).toBeNull()
+        expect(screen.queryByTestId("dense-sign-in-snack-bar")).not.toBeNull()
+
+        // Show the "full" bar after clicking the sign-in button
+        fireEvent.click(screen.getByTestId("dense-sign-in-snack-bar-button"))
+        act(() => {
+            jest.runAllTimers()
+        })
+        expect(screen.queryByTestId("full-sign-in-snack-bar")).not.toBeNull()
+        expect(screen.queryByTestId("dense-sign-in-snack-bar")).toBeNull()
+
+        // Jump to the sign-in page
+        fireEvent.click(screen.getByTestId("full-sign-in-snack-bar-sign-up-button"))
+    })
+})
