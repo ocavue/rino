4be6ae47dd86b5e8595281940b27db00585abd96
ocavue <ocavue@gmail.com>
2020-06-27 04:00:59 +0800

    resetBrowser

--- packages/web/tests/e2e/2.web-redirect.spec.ts
+++ packages/web/tests/e2e/2.web-redirect.spec.ts
@@ -1,8 +1,9 @@
 import { goto, wait } from "./utils"
 
 test("/web", async () => {
+    await jestPuppeteer.resetBrowser()
     await goto("/web")
-    await wait("landing", { timeout: 30000 })
+    await wait("landing", { timeout: 20000 })
     const url = new URL(page.url())
     expect(url.pathname).toEqual("/")
 })
