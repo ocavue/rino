c65b1493bb9f5b330e2ee56a3b31ca377331b845
ocavue <ocavue@gmail.com>
2020-06-27 02:55:40 +0800

    fix tests

--- packages/web/tests/e2e/0.smoke.spec.ts
+++ packages/web/tests/e2e/0.smoke.spec.ts
@@ -1,24 +1,37 @@
+import { login } from "./actions"
 import { goto, wait } from "./utils"
 
 beforeAll(async () => {
     await goto("/")
 })
 
-describe("Check main components", function () {
-    test("Drawer", async () => {
+describe("before sign-in", function () {
+    test("landing page", async () => {
         await wait("sidebar")
-    })
-    test("Appbar", async () => {
-        await wait("appbar")
-    })
-    test("Main", async () => {
-        await wait("main")
     })
 })
 
-describe("Check HTML header", function () {
-    test("title", async () => {
-        await wait("main") // Wait the page
-        expect(await page.title()).toEqual("Rino")
+describe("after sign-in", function () {
+    test("sign-in", async () => {
+        await login()
+    })
+
+    describe("Check main components", function () {
+        test("Drawer", async () => {
+            await wait("sidebar")
+        })
+        test("Appbar", async () => {
+            await wait("appbar")
+        })
+        test("Main", async () => {
+            await wait("main")
+        })
+    })
+
+    describe("Check HTML header", function () {
+        test("title", async () => {
+            await wait("main") // Wait the page
+            expect(await page.title()).toEqual("Rino")
+        })
     })
 })
