93786270eeee4aed37a937a800e6f1b999f5c445
ocavue <ocavue@gmail.com>
2020-06-27 03:13:09 +0800

    add buttons

--- packages/web/src/views/Landing.tsx
+++ packages/web/src/views/Landing.tsx
@@ -1,5 +1,26 @@
+import { Box, Button } from "@material-ui/core"
+import NextLink from "next/link"
 import React from "react"
 
+import Center from "./Center"
+
 export default function Landing() {
-    return <div data-testid="landing">this is landing page</div>
+    return (
+        <Center>
+            <h1>Welcome to Rino</h1>
+
+            <Box>
+                <NextLink href="/sign-up">
+                    <Button component="a" size="large" style={{ marginRight: 16 }}>
+                        Sign In
+                    </Button>
+                </NextLink>
+                <NextLink href="/sign-up">
+                    <Button component="a" size="large" color="primary" variant="contained">
+                        Sign Up
+                    </Button>
+                </NextLink>
+            </Box>
+        </Center>
+    )
 }
