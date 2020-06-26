9e62caee416cec30c3761c4e0b37a9bf59c63008
ocavue <ocavue@gmail.com>
2020-06-27 03:47:45 +0800

    timeout

--- packages/web/tests/e2e/2.web-redirect.spec.ts
+++ packages/web/tests/e2e/2.web-redirect.spec.ts
@@ -2,7 +2,7 @@ import { goto, wait } from "./utils"
 
 test("/web", async () => {
     await goto("/web")
-    await wait("landing")
+    await wait("landing", { timeout: 20000 })
     const url = new URL(page.url())
     expect(url.pathname).toEqual("/")
 })
