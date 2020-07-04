eedb6532c6223b002cea56f76f5714f539847b96
ocavue <ocavue@gmail.com>
2020-07-04 18:58:49 +0800

    fix

--- packages/home/src/__test__/Homepage.spec.tsx
+++ packages/home/src/__test__/Homepage.spec.tsx
@@ -8,11 +8,11 @@ test("homepage", async () => {
 
     await waitFor(() => screen.getByTestId("homepage_root"))
 
-    expect(screen.getByTestId("homepage_appbar_mobile_menu")).not.toBeVisible()
+    await waitFor(() => expect(screen.getByTestId("homepage_appbar_mobile_menu")).not.toBeVisible())
 
     fireEvent.click(screen.getByTestId("homepage_appbar_more"))
-    expect(screen.getByTestId("homepage_appbar_mobile_menu")).toBeVisible()
+    await waitFor(() => expect(screen.getByTestId("homepage_appbar_mobile_menu")).toBeVisible())
 
     fireEvent.click(screen.getByTestId("homepage_appbar_mobile_menu_btn_close"))
-    expect(screen.getByTestId("homepage_appbar_mobile_menu")).not.toBeVisible()
+    await waitFor(() => expect(screen.getByTestId("homepage_appbar_mobile_menu")).not.toBeVisible())
 })
