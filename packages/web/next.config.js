2bcbd2c24c6056dcf6a92cd4254c7866422d521f
ocavue <ocavue@gmail.com>
2020-07-18 21:25:36 +0800

    chore: update next.js configuration

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -4,58 +4,51 @@ const path = require("path")
 const execSync = require("child_process").execSync
 const dotenv = require("dotenv")
 
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
+// const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
 const withOffline = require("next-offline")
 const withImages = require("next-images")
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
     enabled: process.env.ANALYZE === "true",
 })
 
-const allEnvVars = {
-    ...process.env,
-    ...(fs.existsSync(".env") ? dotenv.parse(fs.readFileSync(".env")) : {}),
-    REACT_APP_VERSION: JSON.parse(fs.readFileSync("./package.json")).version,
-    REACT_APP_COMMIT: `${execSync("git rev-parse --short HEAD")}`.trim(),
-}
-const appEnvVars = Object.keys(allEnvVars)
-    .filter((key) => {
-        return (
-            key.startsWith("REACT_APP") ||
-            ["FIREBASE_DATABASE_EMULATOR_HOST", "FIRESTORE_EMULATOR_HOST"].includes(key)
-        )
-    })
-    .reduce((obj, key) => {
-        obj[key] = allEnvVars[key]
-        return obj
-    }, {})
+const envs = (() => {
+    const allEnvs = {
+        ...process.env,
+        ...(fs.existsSync(".env") ? dotenv.parse(fs.readFileSync(".env")) : {}),
+        REACT_APP_VERSION: JSON.parse(fs.readFileSync("./package.json")).version,
+        REACT_APP_COMMIT: `${execSync("git rev-parse --short HEAD")}`.trim(),
+    }
+    const exported = {}
+    for (let key of Object.keys(allEnvs)) {
+        if (
+            key === "FIREBASE_DATABASE_EMULATOR_HOST" ||
+            key === "FIRESTORE_EMULATOR_HOST" ||
+            key.startsWith("REACT_APP")
+        ) {
+            exported[key] = allEnvs[key]
+        }
+    }
+    return exported
+})()
 
 const nextConfig = {
     // Next config
     exportTrailingSlash: true,
     devIndicators: {
         // Disable Next.js's prerender icon in the right bottom corner
-        // of the screen, which cover the sign-in snackbar.
+        // of the screen.
         autoPrerender: false,
     },
     webpack: (config, options) => {
-        // https://github.com/zeit/next.js/issues/7935
-        // https://github.com/zeit/next.js/issues/7779
-        if (config.resolve.plugins) {
-            config.resolve.plugins.push(new TsconfigPathsPlugin())
-        } else {
-            config.resolve.plugins = [new TsconfigPathsPlugin()]
-        }
-
         config.module.rules.push({
             test: /\.(md|txt)$/i,
             use: ["raw-loader"],
         })
-
         return config
     },
-    env: appEnvVars,
+    env: envs,
 
-    // next-image config
+    // next-images config
     esModule: true,
     exclude: path.resolve(__dirname, "src/assets/svg/"),
 }
