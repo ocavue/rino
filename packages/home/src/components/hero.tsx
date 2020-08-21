cca4af6e56a3f3df92ec37f06da696b240437c69
ocavue <ocavue@gmail.com>
2020-08-21 21:47:31 +0800

    add explicit image size

--- packages/home/src/components/hero.tsx
+++ packages/home/src/components/hero.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 // Import a large image as base64 string is a bad practice!
-import snapshot from "../static/snapshot_mac.png"
+import snapshot from "../static/snapshot-mac-1317x762.png"
 
 export const Hero: React.FC = () => (
     <div style={{ marginTop: "2.5rem" }}>
