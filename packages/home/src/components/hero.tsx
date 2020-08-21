05094586c2b2dfc9f37c72c5e49d6b06e74ea3d9
ocavue <ocavue@gmail.com>
2020-08-21 22:27:08 +0800

    move snapshot

--- packages/home/src/components/hero.tsx
+++ packages/home/src/components/hero.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 // Import a large image as base64 string is a bad practice!
-import snapshot from "../static/snapshot-mac-1317x762.png"
+import snapshot from "../../public/share/snapshot-mac-1317x762.png"
 
 export const Hero: React.FC = () => (
     <div style={{ marginTop: "2.5rem" }}>
