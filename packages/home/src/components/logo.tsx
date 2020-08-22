e492ae49f59cab8331a8a3ebc12317d7d88de225
ocavue <ocavue@gmail.com>
2020-08-22 15:17:18 +0800

    add width and height

--- packages/home/src/components/logo.tsx
+++ packages/home/src/components/logo.tsx
@@ -3,5 +3,5 @@ import React from "react"
 import logo from "../../../web/public/img/icons/safari-pinned-tab.svg"
 
 export const Logo: React.FC<{ className?: string }> = (props) => (
-    <img {...props} src={logo} alt="Rino" />
+    <img {...props} src={logo} alt="Rino" width="1024" height="1024" />
 )
