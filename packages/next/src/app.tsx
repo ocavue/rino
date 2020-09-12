298cc20c757d6d27d5125c205d1b513131688b7e
ocavue <ocavue@gmail.com>
2020-09-12 19:08:27 +0800

    review

--- packages/next/src/app.tsx
+++ packages/next/src/app.tsx
@@ -1,15 +1,6 @@
 import Head from "next/head"
 import React from "react"
 
-export function getAppHead() {
-    return (
-        <Head>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />{" "}
-            <title>Rino</title>
-        </Head>
-    )
-}
-
 export const BaseApp: React.FC = ({ children }) => {
     return (
         <>
