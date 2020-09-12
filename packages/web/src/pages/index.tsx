c3aaad232400c7af1d99bb296555b3dc7c17d433
ocavue <ocavue@users.noreply.github.com>
2020-09-12 21:24:54 +0800

    fix(web): remove the unexpected 404 page when redirecting (#368)

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,5 +1,3 @@
-import { getHomeHostName } from "@rino.app/common"
-
 import { DynamicPage } from "src/utils"
 
 function isSignedIn(): boolean {
@@ -16,18 +14,10 @@ function isSignedIn(): boolean {
 
 export default DynamicPage("Index", () => {
     if (isSignedIn()) {
-        // TODO: don't need to use import here
         return import("src/views/Workspace")
     } else {
         // This is weird because the server should return a 302 response and the client don't need
         // to do the redirect job.
-        console.warn("not login")
-        const homeHost = getHomeHostName({ protocol: true })
-        console.log(`redirecting to ${homeHost}`)
-
-        // window.location.replace(...) is better than using window.location.href, because replace()
-        // does not keep the originating page in the session history, meaning the user won't get
-        // stuck in a never-ending back-button fiasco.
-        window.location.replace(homeHost)
+        return import("src/views/Redirect")
     }
 })
---
+++ packages/web/src/views/Redirect.tsx
@@ -0,0 +1,22 @@
+/* istanbul ignore file */
+import { getHomeHostName } from "@rino.app/common"
+import React from "react"
+
+import Center from "./Center"
+
+export default function Redirect() {
+    console.warn("not login")
+    const homeHost = getHomeHostName({ protocol: true })
+    setTimeout(() => {
+        console.log(`redirecting to ${homeHost}`)
+        // window.location.replace(...) is better than using window.location.href, because replace()
+        // does not keep the originating page in the session history, meaning the user won't get
+        // stuck in a never-ending back-button fiasco.
+        window.location.replace(homeHost)
+    }, 0)
+    return (
+        <Center>
+            <div>redirecting to {homeHost}</div>
+        </Center>
+    )
+}
