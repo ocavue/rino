bc95c500e36214205fc896f519f9cb34ee2c3306
ocavue <ocavue@gmail.com>
2020-09-12 17:48:03 +0800

    add isSignedIn

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -2,20 +2,32 @@ import { getHomeHostName } from "@rino.app/common"
 
 import { DynamicPage } from "src/utils"
 
+function isSignedIn(): boolean {
+    if (typeof localStorage !== "undefined") {
+        if (localStorage?.getItem("__rino_dev_auth_state") === "yes") {
+            return true
+        }
+    }
+    if (process.env.NODE_ENV === "development" || process.env.REACT_APP_TESTING) {
+        return true
+    }
+    return false
+}
+
 export default DynamicPage("Index", () => {
-    if (localStorage?.getItem("__rino_dev_auth_state") === "yes") {
+    if (isSignedIn()) {
         // TODO: don't need to use import here
         return import("src/views/Workspace")
     } else {
-        // This is weird because the server should return a 302 response and the client don't need to do the redirect job.
+        // This is weird because the server should return a 302 response and the client don't need
+        // to do the redirect job.
         console.warn("not login")
         const homeHost = getHomeHostName({ protocol: true })
         console.log(`redirecting to ${homeHost}`)
-        // TODO: how to sign-in in lighthouse testing.
 
-        // window.location.replace(...) is better than using window.location.href, because replace() does
-        // not keep the originating page in the session history, meaning the user won't get stuck in a
-        // never-ending back-button fiasco.
+        // window.location.replace(...) is better than using window.location.href, because replace()
+        // does not keep the originating page in the session history, meaning the user won't get
+        // stuck in a never-ending back-button fiasco.
         window.location.replace(homeHost)
     }
 })
