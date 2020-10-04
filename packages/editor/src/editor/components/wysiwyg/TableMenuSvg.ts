8b2989cffd39f8623473ee3502b464d6193d6187
ocavue <ocavue@gmail.com>
2020-10-04 11:03:11 +0800

    svg

--- packages/editor/src/editor/components/wysiwyg/TableMenuSvg.ts
+++ packages/editor/src/editor/components/wysiwyg/TableMenuSvg.ts
@@ -1,8 +1,8 @@
-import AddColumnAfter from "../../../../src/assets/svg/add_column_after.svg"
-import AddColumnBefore from "../../../../src/assets/svg/add_column_before.svg"
-import AddRowAfter from "../../../../src/assets/svg/add_row_after.svg"
-import AddRowBefore from "../../../../src/assets/svg/add_row_before.svg"
-import DeleteColumn from "../../../../src/assets/svg/delete_column.svg"
-import DeleteRow from "../../../../src/assets/svg/delete_row.svg"
+import AddColumnAfter from "../../../assets/svg/add_column_after.svg"
+import AddColumnBefore from "../../../assets/svg/add_column_before.svg"
+import AddRowAfter from "../../../assets/svg/add_row_after.svg"
+import AddRowBefore from "../../../assets/svg/add_row_before.svg"
+import DeleteColumn from "../../../assets/svg/delete_column.svg"
+import DeleteRow from "../../../assets/svg/delete_row.svg"
 
 export { AddColumnAfter, AddColumnBefore, AddRowAfter, AddRowBefore, DeleteColumn, DeleteRow }
