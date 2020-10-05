255a67804f691b6e867a47a8b5b8bad9dd26f7bf
ocavue <ocavue@gmail.com>
2020-10-05 17:15:44 +0800

    update

---
+++ scripts/slugify.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+# https://stackoverflow.com/a/63286099
+slugify () {
+    echo "$1" | iconv -c -t ascii//TRANSLIT | sed -E 's/[~^]+//g' | sed -E 's/[^a-zA-Z0-9]+/-/g' | sed -E 's/^-+|-+$//g' | tr A-Z a-z
+}
+
+slugify $1
