b1b44f5fc0c8acaa31afef45c3b7cb9923e1ed99
ocavue <ocavue@gmail.com>
2020-10-13 02:26:55 +0800

    update

---
+++ scripts/slugify-branch.js
@@ -0,0 +1,13 @@
+const { execSync } = require("child_process")
+
+const branch = execSync("git rev-parse --abbrev-ref HEAD", { encoding: "utf-8" })
+const slugify = branch
+    .replace(/[^a-zA-Z0-9]+/g, "-")
+    .replace(/^-+/g, "")
+    .replace(/-+$/g, "")
+    .toLocaleLowerCase()
+
+if (require.main === module) {
+    console.log(slugify)
+}
+exports = branch
