70377273117b55877aac2a5bb416c6c9e32e08e7
ocavue <ocavue@gmail.com>
2020-10-30 13:53:52 +0800

    fix

--- packages/home/src/hooks.ts
+++ packages/home/src/hooks.ts
@@ -1,13 +1,39 @@
-import { getWebAppHostName } from "@rino.app/common"
-import { useMemo } from "react"
+import { getSignInState, getWebAppHostName } from "@rino.app/common"
+import { useEffect, useMemo, useState } from "react"
 
-export function useAuthLinks() {
-    return useMemo(() => {
-        const host = getWebAppHostName()
-        return {
-            signInLink: `${host}/sign-in`,
-            signUpLink: `${host}/sign-up`,
-            appLink: host,
-        }
+type AuthInfo =
+    | {
+          isSSR: true
+      }
+    | {
+          isSSR: false
+          signInLink: string
+          signUpLink: string
+          appLink: string
+          isSignedIn: boolean
+      }
+
+export function useAuthInfo(): AuthInfo {
+    const [isSignedIn, setIsSignedIn] = useState<boolean | null>(null)
+
+    useEffect(() => {
+        setIsSignedIn(getSignInState())
     }, [])
+
+    return useMemo(() => {
+        if (isSignedIn === null) {
+            return {
+                isSSR: true,
+            } as const
+        } else {
+            const host = getWebAppHostName()
+            return {
+                signInLink: `${host}/sign-in`,
+                signUpLink: `${host}/sign-up`,
+                appLink: host,
+                isSignedIn,
+                isSSR: false,
+            } as const
+        }
+    }, [isSignedIn])
 }
