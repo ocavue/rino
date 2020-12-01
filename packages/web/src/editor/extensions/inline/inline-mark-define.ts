a87f00bccb86268b8be7138d45d9cc9733a59569
ocavue <ocavue@gmail.com>
2020-12-01 16:28:53 +0800

    fix(web): correct eslint-typescript warning

--- packages/web/src/editor/extensions/inline/inline-mark-define.ts
+++ packages/web/src/editor/extensions/inline/inline-mark-define.ts
@@ -9,189 +9,187 @@ const endpointAttrs = {
     end: { default: false },
 }
 
-const RinoMarkExtensionClasses = [
-    class MetaKey extends MarkExtension {
-        static disableExtraAttributes = true
-        get name() {
-            return "mdMark" as const
+class MetaKey extends MarkExtension {
+    static disableExtraAttributes = true
+    get name() {
+        return "mdMark" as const
+    }
+    createMarkSpec(): MarkExtensionSpec {
+        return {
+            inclusive: false,
+            attrs: endpointAttrs,
+            toDOM: (mark, inline) => ["span", { class: "md-mark" }, 0],
         }
-        createMarkSpec(): MarkExtensionSpec {
-            return {
-                inclusive: false,
-                attrs: endpointAttrs,
-                toDOM: (mark, inline) => ["span", { class: "md-mark" }, 0],
-            }
-        }
-    },
+    }
+}
 
-    class PlainText extends MarkExtension {
-        static disableExtraAttributes = true
-        get name() {
-            return "mdText" as const
+class PlainText extends MarkExtension {
+    static disableExtraAttributes = true
+    get name() {
+        return "mdText" as const
+    }
+    createMarkSpec(): MarkExtensionSpec {
+        return {
+            attrs: endpointAttrs,
+            toDOM: (mark, inline) => ["span", 0],
         }
-        createMarkSpec(): MarkExtensionSpec {
-            return {
-                attrs: endpointAttrs,
-                toDOM: (mark, inline) => ["span", 0],
-            }
-        }
-    },
+    }
+}
 
-    class Emphasis extends MarkExtension {
-        static disableExtraAttributes = true
-        get name() {
-            return "mdEm" as const
+class Emphasis extends MarkExtension {
+    static disableExtraAttributes = true
+    get name() {
+        return "mdEm" as const
+    }
+    createMarkSpec(): MarkExtensionSpec {
+        return {
+            attrs: commonAttrs,
+            toDOM: (mark, inline) => ["em", 0],
         }
-        createMarkSpec(): MarkExtensionSpec {
-            return {
-                attrs: commonAttrs,
-                toDOM: (mark, inline) => ["em", 0],
-            }
-        }
-    },
+    }
+}
 
-    class Strong extends MarkExtension {
-        static disableExtraAttributes = true
-        get name() {
-            return "mdStrong" as const
+class Strong extends MarkExtension {
+    static disableExtraAttributes = true
+    get name() {
+        return "mdStrong" as const
+    }
+    createMarkSpec(): MarkExtensionSpec {
+        return {
+            attrs: commonAttrs,
+            toDOM: (mark, inline) => ["strong", 0],
         }
-        createMarkSpec(): MarkExtensionSpec {
-            return {
-                attrs: commonAttrs,
-                toDOM: (mark, inline) => ["strong", 0],
-            }
-        }
-    },
+    }
+}
 
-    class CodeText extends MarkExtension {
-        static disableExtraAttributes = true
-        get name() {
-            return "mdCodeText" as const
+class CodeText extends MarkExtension {
+    static disableExtraAttributes = true
+    get name() {
+        return "mdCodeText" as const
+    }
+    createMarkSpec(): MarkExtensionSpec {
+        return {
+            attrs: commonAttrs,
+            toDOM: (mark, inline) => ["code", 0],
         }
-        createMarkSpec(): MarkExtensionSpec {
-            return {
-                attrs: commonAttrs,
-                toDOM: (mark, inline) => ["code", 0],
-            }
-        }
-    },
+    }
+}
 
-    class CodeSapce extends MarkExtension {
-        static disableExtraAttributes = true
-        get name() {
-            return "mdCodeSpace" as const
+class CodeSapce extends MarkExtension {
+    static disableExtraAttributes = true
+    get name() {
+        return "mdCodeSpace" as const
+    }
+    createMarkSpec(): MarkExtensionSpec {
+        return {
+            attrs: commonAttrs,
+            toDOM: (mark, inline) => ["span", 0],
         }
-        createMarkSpec(): MarkExtensionSpec {
-            return {
-                attrs: commonAttrs,
-                toDOM: (mark, inline) => ["span", 0],
-            }
-        }
-    },
+    }
+}
 
-    class Delete extends MarkExtension {
-        static disableExtraAttributes = true
-        get name() {
-            return "mdDel" as const
+class Delete extends MarkExtension {
+    static disableExtraAttributes = true
+    get name() {
+        return "mdDel" as const
+    }
+    createMarkSpec(): MarkExtensionSpec {
+        return {
+            attrs: commonAttrs,
+            toDOM: (mark, inline) => ["del", 0],
         }
-        createMarkSpec(): MarkExtensionSpec {
-            return {
-                attrs: commonAttrs,
-                toDOM: (mark, inline) => ["del", 0],
-            }
-        }
-    },
+    }
+}
 
-    class LinkText extends MarkExtension {
-        static disableExtraAttributes = true
-        get name() {
-            return "mdLinkText" as const
-        }
-        createMarkSpec(): MarkExtensionSpec {
-            return {
-                attrs: {
-                    ...commonAttrs,
-                    href: {
-                        default: "",
-                    },
+class LinkText extends MarkExtension {
+    static disableExtraAttributes = true
+    get name() {
+        return "mdLinkText" as const
+    }
+    createMarkSpec(): MarkExtensionSpec {
+        return {
+            attrs: {
+                ...commonAttrs,
+                href: {
+                    default: "",
                 },
-                toDOM: (mark) => [
-                    "span",
-                    [
-                        "a",
-                        {
-                            href: mark.attrs.href,
-                            // This <a> element is `contenteditable`, so it's not clickable by default.
-                            onClick: `window.open('${mark.attrs.href}')`,
-                        },
-                        0,
-                    ],
+            },
+            toDOM: (mark) => [
+                "span",
+                [
+                    "a",
+                    {
+                        href: mark.attrs.href,
+                        // This <a> element is `contenteditable`, so it's not clickable by default.
+                        onClick: `window.open('${mark.attrs.href}')`,
+                    },
+                    0,
                 ],
-            }
+            ],
         }
-    },
+    }
+}
 
-    class LinkUri extends MarkExtension {
-        static disableExtraAttributes = true
-        get name() {
-            return "mdLinkUri" as const
+class LinkUri extends MarkExtension {
+    static disableExtraAttributes = true
+    get name() {
+        return "mdLinkUri" as const
+    }
+    createMarkSpec(): MarkExtensionSpec {
+        return {
+            spanning: false,
+            attrs: commonAttrs,
+            toDOM: (mark, inline) => ["a", { class: "md-link" }, 0],
         }
-        createMarkSpec(): MarkExtensionSpec {
-            return {
-                spanning: false,
-                attrs: commonAttrs,
-                toDOM: (mark, inline) => ["a", { class: "md-link" }, 0],
-            }
-        }
-    },
+    }
+}
 
-    class ImgText extends MarkExtension {
-        static disableExtraAttributes = true
-        get name() {
-            return "mdImgText" as const
+class ImgText extends MarkExtension {
+    static disableExtraAttributes = true
+    get name() {
+        return "mdImgText" as const
+    }
+    createMarkSpec(): MarkExtensionSpec {
+        return {
+            spanning: false,
+            attrs: commonAttrs,
+            toDOM: (mark, inline) => ["span", { class: "md-img-text" }, 0],
         }
-        createMarkSpec(): MarkExtensionSpec {
-            return {
-                spanning: false,
-                attrs: commonAttrs,
-                toDOM: (mark, inline) => ["span", { class: "md-img-text" }, 0],
-            }
-        }
-    },
+    }
+}
 
-    class ImgUri extends MarkExtension {
-        static disableExtraAttributes = true
-        get name() {
-            return "mdImgUri" as const
-        }
-        createMarkSpec(): MarkExtensionSpec {
-            return {
-                spanning: false,
-                attrs: {
-                    ...commonAttrs,
-                    href: {
-                        default: "",
-                    },
+class ImgUri extends MarkExtension {
+    static disableExtraAttributes = true
+    get name() {
+        return "mdImgUri" as const
+    }
+    createMarkSpec(): MarkExtensionSpec {
+        return {
+            spanning: false,
+            attrs: {
+                ...commonAttrs,
+                href: {
+                    default: "",
                 },
-            }
+            },
         }
-        createNodeViews = (): NodeViewMethod => {
-            return (mark, view): NodeView => {
-                const innerContainer = document.createElement("span")
+    }
+    createNodeViews = (): NodeViewMethod => {
+        return (mark, view): NodeView => {
+            const innerContainer = document.createElement("span")
 
-                const img = document.createElement("img")
-                img.setAttribute("src", mark.attrs.href)
+            const img = document.createElement("img")
+            img.setAttribute("src", mark.attrs.href)
 
-                const outerContainer = document.createElement("span")
-                outerContainer.appendChild(img)
-                outerContainer.appendChild(innerContainer)
-                outerContainer.setAttribute("class", "md-img-uri")
+            const outerContainer = document.createElement("span")
+            outerContainer.appendChild(img)
+            outerContainer.appendChild(innerContainer)
+            outerContainer.setAttribute("class", "md-img-uri")
 
-                return { dom: outerContainer, contentDOM: innerContainer }
-            }
+            return { dom: outerContainer, contentDOM: innerContainer }
         }
-    },
-]
+    }
+}
 
 const autoHideMarks: Record<string, true> = {
     mdMark: true,
@@ -204,6 +202,19 @@ export function isAutoHideMark(name?: string): boolean {
     return autoHideMarks[name]
 }
 
-export const rinoMarkExtensions = RinoMarkExtensionClasses.map((Ext) => new Ext())
+const rinoMarkExtensionClasses = [
+    MetaKey,
+    PlainText,
+    Emphasis,
+    Strong,
+    CodeText,
+    CodeSapce,
+    Delete,
+    LinkText,
+    LinkUri,
+    ImgText,
+    ImgUri,
+]
+export const rinoMarkExtensions = rinoMarkExtensionClasses.map((Ext) => new Ext())
 export type RinoMarkExtension = typeof rinoMarkExtensions[number]
 export type RinoMarkName = RinoMarkExtension["name"]
