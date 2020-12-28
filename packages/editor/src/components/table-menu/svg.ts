5cf04240a7f2f87b75c22765192345bcfe979c7d
ocavue <ocavue@gmail.com>
2020-12-28 13:41:35 +0800

    fix

--- packages/editor/src/components/table-menu/svg.ts
+++ packages/editor/src/components/table-menu/svg.ts
@@ -5,18 +5,7 @@ import AddRowBefore from "../../assets/svg/AddRowBefore"
 import DeleteColumn from "../../assets/svg/DeleteColumn"
 import DeleteRow from "../../assets/svg/DeleteRow"
 
-type TableMenuSvg = React.FC<{
-    alt?: string
-    width?: string
-    height?: string
-}>
-
-export type TableMenuSvgs = Record<
-    "AddColumnAfter" | "AddColumnBefore" | "AddRowAfter" | "AddRowBefore" | "DeleteColumn" | "DeleteRow",
-    TableMenuSvg
->
-
-export const tableMenuSvgs: TableMenuSvgs = {
+export const tableMenuSvgs = {
     AddColumnBefore,
     AddColumnAfter,
     AddRowAfter,
