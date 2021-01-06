2a9c52053b0f89e716135885fbd84c8abd7a8afe
ocavue <ocavue@users.noreply.github.com>
2021-01-06 13:26:37 +0800

    chore: update vercel script (#481)

--- scripts/clean_vercel_deployment.sh
+++ scripts/clean_vercel_deployment.sh
@@ -1,15 +1,32 @@
+#!/usr/bin/env bash
 
-# This scrript can clean useless deployment in Vercel. You may need to run this script multiple times since
-# it can only delete limit deployments.
+# This script can clean deployments in Vercel that
+# don't an active alias. You may or may not need to
+# run this script multiple times.
+#
+# Before running this script, You need to run `npm i -g vercel`
+# to install the command line tool `vercel` and run `vercel login`
+# to login the vercel account.
 
 set -ex
 
-echo "" > /tmp/clean_vercel_deployment.txt
+tmpfile="/tmp/clean_vercel_deployment.txt"
+touch $tmpfile
 
-for project_name in "rino-home" "rino-web" "rino-server"; do
-    vercel ls $project_name | egrep -o "$project_name-[a-zA-Z0-9]+.vercel.app" >> /tmp/clean_vercel_deployment.txt
+clean() {
+    echo "" > $tmpfile
+
+    for project_name in "rino-home" "rino-web" "rino-server"; do
+        vercel ls $project_name | egrep -o "$project_name-[a-zA-Z0-9]+.vercel.app" >> $tmpfile
+    done
+
+    cat $tmpfile
+
+    vercel remove --yes --safe $(cat $tmpfile | sort --random-sort | head -n 20 | paste -sd ' ' -)
+}
+
+clean
+until [ "$(cat $tmpfile | wc -l)" -lt "10" ]; do
+    sleep 20
+    clean
 done
-
-cat /tmp/clean_vercel_deployment.txt
-
-vercel remove --yes --safe $(cat /tmp/clean_vercel_deployment.txt | head -n 20 | paste -sd ' ' -)
