d1f4eba8351d37d36b4b8cfc997566efb751a82f
ocavue <ocavue@users.noreply.github.com>
2021-01-06 16:09:36 +0800

    feat(web): adjust the editor width (#482)

---
+++ .changeset/dirty-gorillas-juggle.md
@@ -0,0 +1,5 @@
+---
+"@rino.app/web": patch
+---
+
+Adjust the editor width.
--- packages/web/src/constants.ts
+++ packages/web/src/constants.ts
@@ -5,7 +5,7 @@ export const APPBAR_ICON_BUTTON_SIZE = 40
 export const APPBAR_ICON_SIZE = 24
 export const APPBAR_ICON_BUTTON_MARGIN = 12
 export const APPBAR_HEIGHT = APPBAR_ICON_BUTTON_SIZE + APPBAR_ICON_BUTTON_MARGIN * 2
-export const MAX_EDITOR_WIDTH = 1024
+export const MAX_EDITOR_WIDTH = 960
 
 // Time (millisecond)
 export const SIGN_IN_SNACKBAR_SHOW_DELAY = 3000
