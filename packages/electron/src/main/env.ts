c6439ce715065ec545432b6f88d318c00948fa5f
ocavue <ocavue@gmail.com>
2021-04-23 16:42:43 +0800

    feat(electron): update the release workflow

--- packages/electron/src/main/env.ts
+++ packages/electron/src/main/env.ts
@@ -3,7 +3,7 @@ const NODE_ENV = process.env.NODE_ENV
 const env = {
     NODE_ENV: NODE_ENV,
     MODE: NODE_ENV,
-    PROD: NODE_ENV === "production" || NODE_ENV === undefined,
+    PROD: NODE_ENV === "production" || !NODE_ENV,
 }
 
 export { env }
