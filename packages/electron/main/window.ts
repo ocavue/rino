333268d012a80573066d034bba49fe14473cf5e2
ocavue <ocavue@gmail.com>
2021-06-08 00:40:10 +0800

    feat(electron): open files with double-click

--- packages/electron/main/window.ts
+++ packages/electron/main/window.ts
@@ -1,4 +1,4 @@
-import { BrowserWindow } from "electron"
+import { app, BrowserWindow } from "electron"
 import { join } from "path"
 import { URL } from "url"
 
@@ -8,51 +8,67 @@ import { logger } from "./logger"
 const windows = new Set<BrowserWindow>()
 
 export async function createWindow() {
-    let x: number | undefined, y: number | undefined
-    const currentWindow = BrowserWindow.getFocusedWindow()
-    if (currentWindow) {
-        const [currentWindowX, currentWindowY] = currentWindow.getPosition()
-        x = currentWindowX + 32
-        y = currentWindowY + 32
+    try {
+        logger.info("creating new window")
+        await app.whenReady()
+
+        let x: number | undefined, y: number | undefined
+        const currentWindow = BrowserWindow.getFocusedWindow()
+        if (currentWindow) {
+            const [currentWindowX, currentWindowY] = currentWindow.getPosition()
+            x = currentWindowX + 32
+            y = currentWindowY + 32
+        }
+
+        const newWindow = new BrowserWindow({
+            x,
+            y,
+            show: false,
+            webPreferences: {
+                preload: join(__dirname, "../../preload/dist/index.js"),
+            },
+        })
+
+        newWindow.once("ready-to-show", () => {
+            newWindow?.show()
+        })
+
+        newWindow.on("closed", () => {
+            windows.delete(newWindow)
+        })
+
+        if (env.IS_DEV) {
+            setTimeout(() => {
+                newWindow?.webContents.openDevTools({ activate: false, mode: "detach" })
+            }, 1000)
+        }
+
+        /**
+         * URL for main window.
+         * Vite dev server for development.
+         * `file://../renderer/index.html` for production and test
+         */
+        const pageUrl = env.IS_DEV ? "http://localhost:3004" : new URL("../renderer/dist/index.html", "file://" + __dirname).toString()
+        logger.info(`loading ${pageUrl}`)
+
+        await newWindow.loadURL(pageUrl)
+        logger.info("created new window")
+
+        return newWindow
+    } catch (error) {
+        logger.error("failed to create new window:", error)
+        throw error
     }
-
-    const newWindow = new BrowserWindow({
-        x,
-        y,
-        show: false,
-        webPreferences: {
-            preload: join(__dirname, "../../preload/dist/index.js"),
-        },
-    })
-
-    newWindow.once("ready-to-show", () => {
-        newWindow?.show()
-    })
-
-    newWindow.on("closed", () => {
-        windows.delete(newWindow)
-    })
-
-    if (env.IS_DEV) {
-        setTimeout(() => {
-            newWindow?.webContents.openDevTools({ activate: false, mode: "detach" })
-        }, 1000)
-    }
-
-    /**
-     * URL for main window.
-     * Vite dev server for development.
-     * `file://../renderer/index.html` for production and test
-     */
-    const pageUrl = env.IS_DEV ? "http://localhost:3004" : new URL("../renderer/dist/index.html", "file://" + __dirname).toString()
-    logger.info(`loading ${pageUrl}`)
-
-    await newWindow.loadURL(pageUrl)
-    return newWindow
 }
 
 export async function createWindowByOpeningFile(path: string) {
     if (!path) return
     const win = await createWindow()
-    win.webContents.send("send:openFile", { path })
+    win?.webContents.send("send:openFile", { path })
+}
+
+export async function createWindowIfNotExist() {
+    if (windows.size === 0) {
+        await createWindow()
+    }
 }
