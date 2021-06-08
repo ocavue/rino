bb425df8a5a1a795339a43d5eae079957c4b1b9d
ocavue <ocavue@users.noreply.github.com>
2021-06-08 11:27:44 +0000

    fix

--- packages/electron/renderer/vite.config.ts
+++ packages/electron/renderer/vite.config.ts
@@ -4,12 +4,27 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "",
+    cacheDir: ".cache/vite",
     plugins: [reactRefresh()],
     server: {
         port: 3004,
         hmr: false, // it seems that electron doesn't play well with vite's hmr
     },
     build: {
+        /*
+        test in 2021-06-08
+
+        | sourcemap | minify  | size | time |
+        | --------- | ------- | ---- | ---- |
+        | false     | esbuild | 1.4M | 14s  |
+        | false     | terser  | 1.3M | 27s  |
+        | false     | false   | 2.7M | 16s  |
+        | true      | esbuild | 6.5M | 15s  |
+        | true      | terser  | 6.7M | 29s  |
+        | true      | false   | 8.1M | 18s  |
+
+        */
+        minify: "esbuild",
         sourcemap: true,
         chunkSizeWarningLimit: 5000,
     },
