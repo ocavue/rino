b8e29f97d991e797db67c38068045a017683f3eb
ocavue <ocavue@gmail.com>
2021-06-15 22:18:54 +0800

    chore: add clean script

--- packages/editor/playground/vite.config.ts
+++ packages/editor/playground/vite.config.ts
@@ -1,9 +1,10 @@
-import reactRefresh from "@vitejs/plugin-react-refresh"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig((env) => ({
     base: "",
-    plugins: [reactRefresh()],
     server: { port: 3001 },
-})
+    define: {
+        "process.env.NODE_ENV": JSON.stringify(env.mode === "production" ? "production" : "development"),
+    },
+}))
