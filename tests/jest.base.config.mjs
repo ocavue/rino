ab4623c5c0f033f0cd80c24d87d2e1b767464124
ocavue <ocavue@users.noreply.github.com>
2021-06-27 15:40:06 +0800

    chore: use `.mjs` as the extension of the jest configuration files (#672)

--- tests/jest.base.config.mjs
+++ tests/jest.base.config.mjs
@@ -1,17 +1,20 @@
+// @ts-check
+
 // This file stores all shared Jest config for each packages
 
-module.exports = {
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
     // A map from regular expressions to paths to transformers
     transform: {
         ".*\\.ts(x)?$": "babel-jest",
-        ".*\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2|md)$": "jest-transform-stub",
+        ".*\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2|md|svg)$": "jest-transform-stub",
     },
 
     testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 
     // A map from regular expressions to module names that allow to stub out resources with a single module
     moduleNameMapper: {
-        "\\.svg$": `${__dirname}/__mocks__/svgrMock.js`, // I used `__dirname` instead of `<rootDir>` here because `<rootDir>` will be the path of sub-package root.
+        "\\.svg$": `<rootDir>/../../tests/__mocks__/svgrMock.js`,
         "^src/(.*)$": "<rootDir>/src/$1",
         "^tests/(.*)$": "<rootDir>/tests/$1",
     },
@@ -19,3 +22,5 @@ module.exports = {
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
     transformIgnorePatterns: ["/node_modules/"],
 }
+
+export default config
