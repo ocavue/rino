34233d1c4b85d222ab2751ea2bc31385a83c01d7
ocavue <ocavue@gmail.com>
2021-06-27 16:37:18 +0800

    add comment

--- packages/editor/src/components/theme/base.ts
+++ packages/editor/src/components/theme/base.ts
@@ -4,16 +4,33 @@ import { extensionListStyledCss } from "@remirror/styles/emotion"
 export const EDITOR_THEME_BASE = css`
     ${extensionListStyledCss}
 
-    /* disable margin-collapsing https://stackoverflow.com/a/19719427 */
+    /**
+     * disable margin-collapsing https://stackoverflow.com/a/19719427
+     */
     flex-direction: column;
     display: flex;
 
-    /* extend to full height */
+    /**
+     * extend to full height
+     */
     min-height: 100%;
     & .ProseMirror {
         min-height: 100%;
     }
 
+    /**
+     * add padding to the editor
+     *
+     * you may want to add something like this in the theme css：
+     *
+     *   & :first-child {
+     *       margin-top: 0;
+     *   }
+     *
+     *   & :last-child {
+     *       margin-bottom: 0;
+     *   }
+     */
     & .ProseMirror {
         padding-top: 32px;
         padding-left: 32px;
@@ -21,7 +38,10 @@ export const EDITOR_THEME_BASE = css`
         padding-bottom: 64px;
     }
 
+    /**
+     * remove the default outline
+     */
     & .ProseMirror-focused {
-        outline: none; /* remove the default outline */
+        outline: none;
     }
 `
