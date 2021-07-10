7a5b2958ce31d3ad8456e277a0c4721856b6df33
ocavue <ocavue@gmail.com>
2021-07-10 14:42:04 +0800

    fix(home): fix server-side rendering

--- packages/home/src/components/logo.tsx
+++ packages/home/src/components/logo.tsx
@@ -1,9 +1,5 @@
 import React from "react"
 
-import Svg from "../assets/share/img/icons/safari-pinned-tab.svg"
-
-export const Logo: React.FC<{ className?: string }> = (props) => (
-    <div {...props}>
-        <Svg alt="Rino" width="128" height="128" />
-    </div>
-)
+export const Logo: React.FC<{ className?: string }> = (props) => {
+    return <img src="/share/img/icons/safari-pinned-tab.svg" {...props} />
+}
--- packages/next/next.base.config.js
+++ packages/next/next.base.config.js
@@ -30,44 +30,22 @@ function getNextConfig() {
             autoPrerender: false,
         },
         webpack: (config, options) => {
-            config.module.rules.push(
-                {
-                    test: /\.(jpe?g|png|webp)$/i,
-                    use: [
-                        {
-                            loader: "responsive-loader",
-                            options: {
-                                adapter: require("responsive-loader/sharp"),
-                                sizes: [320, 640, 960, 1200, 2400],
-                                placeholder: false,
-                                esModule: true,
-                                outputPath: "static/images/_responsive/",
-                                publicPath: "_next/static/images/_responsive/",
-                            },
+            config.module.rules.push({
+                test: /\.(jpe?g|png|webp)$/i,
+                use: [
+                    {
+                        loader: "responsive-loader",
+                        options: {
+                            adapter: require("responsive-loader/sharp"),
+                            sizes: [320, 640, 960, 1200, 2400],
+                            placeholder: false,
+                            esModule: true,
+                            outputPath: "static/images/_responsive/",
+                            publicPath: "_next/static/images/_responsive/",
                         },
-                    ],
-                },
-                {
-                    test: /\.svg$/i,
-                    use: [
-                        {
-                            loader: "@svgr/webpack",
-                            options: {
-                                svgoConfig: {
-                                    plugins: [
-                                        {
-                                            // SVGO will remove the svg attribute `viewBox` by default,
-                                            // which will stop me from resizing the image by setting
-                                            // `width` and `height`.
-                                            removeViewBox: false,
-                                        },
-                                    ],
-                                },
-                            },
-                        },
-                    ],
-                },
-            )
+                    },
+                ],
+            })
             return config
         },
         env: getEnvs(),
