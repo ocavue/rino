56d3294ff0253c92b87f2be4f62f31cce6de977f
ocavue <ocavue@users.noreply.github.com>
2021-07-16 16:22:44 +0800

    chore: use ts-jest for better ESM support (#716)

--- packages/editor/e2e/jest.config.mjs
+++ packages/editor/e2e/jest.config.mjs
@@ -1,15 +1,12 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
-import baseConfig from "../../tests/jest.base.config.mjs"
+import baseConfig from "../../../tests/jest.base.config.mjs"
 
 export default {
     ...baseConfig,
 
     displayName: { name: "EDITOR:E2E", color: "blackBright" },
-
-    // The glob patterns Jest uses to detect test files
-    testMatch: ["<rootDir>/e2e/*.spec.(js|ts|jsx|tsx)"],
 
     preset: "jest-playwright-preset",
 
