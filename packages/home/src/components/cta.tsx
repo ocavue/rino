6bae705eb931d53b14b87337b9afd1002c607c71
ocavue <ocavue@gmail.com>
2021-07-17 22:56:21 +0800

    bug fix

--- packages/home/src/components/cta.tsx
+++ packages/home/src/components/cta.tsx
@@ -1,11 +1,11 @@
-import { createStyles, makeStyles } from "@material-ui/core"
-import ButtonBase from "@material-ui/core/ButtonBase"
-import Typography from "@material-ui/core/Typography"
+import { Button, createStyles, makeStyles, Typography } from "@material-ui/core"
 import React from "react"
+
+import { rootLevelBlock } from "../styles/layout"
 
 const useStyles = makeStyles((theme) =>
     createStyles({
-        cta: {
+        root: {
             width: "100%",
 
             marginTop: 120,
@@ -13,15 +13,19 @@ const useStyles = makeStyles((theme) =>
             paddingTop: 48,
             paddingBottom: 48,
 
+            background: theme.palette.primary.light,
+        },
+        container: {
+            ...rootLevelBlock,
+
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
-            background: theme.palette.primary.light,
         },
         header: {
             marginTop: 0,
             marginBottom: 32,
-            fontWeight: 700,
+            fontWeight: 500,
             color: theme.palette.common.white,
 
             fontSize: "2.2rem",
@@ -31,28 +35,8 @@ const useStyles = makeStyles((theme) =>
         },
 
         button: {
-            position: "relative",
-            height: 64,
-            width: 240,
-
             border: "2px solid #fff",
-            borderRadius: 4,
             color: "#fff",
-
-            "&:hover, &$focusVisible": {
-                border: "none",
-                zIndex: 1,
-                backgroundColor: "#fff",
-                color: "#000",
-            },
-        },
-        focusVisible: {},
-
-        buttonText: {
-            fontSize: "1.7em",
-            fontWeight: 500,
-            position: "relative",
-            padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
         },
     }),
 )
@@ -62,18 +46,15 @@ export const CTA: React.FC<{ handleOpenDownloadDialog: () => void }> = ({ handle
     const classes = useStyles()
 
     return (
-        <div className={classes.cta}>
-            <h3 className={classes.header}>Download Rino today</h3>
-            <ButtonBase
-                focusRipple
-                className={classes.button}
-                focusVisibleClassName={classes.focusVisible}
-                onClick={handleOpenDownloadDialog}
-            >
-                <Typography component="span" variant="subtitle1" color="inherit" className={classes.buttonText}>
-                    Download Rino
-                </Typography>
-            </ButtonBase>
+        <div className={classes.root}>
+            <div className={classes.container}>
+                <h3 className={classes.header}>Download Rino today</h3>
+                <Button variant="outlined" className={classes.button} onClick={handleOpenDownloadDialog} size="large">
+                    <Typography component="span" variant="h6">
+                        Download Rino
+                    </Typography>
+                </Button>
+            </div>
         </div>
     )
 }
