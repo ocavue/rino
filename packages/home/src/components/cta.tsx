a3ba143b7026e4f1242892b345c64e75d047d6b6
ocavue <ocavue@gmail.com>
2021-07-17 16:13:19 +0800

    download btn

--- packages/home/src/components/cta.tsx
+++ packages/home/src/components/cta.tsx
@@ -2,22 +2,30 @@ import { Box, Button, createStyles, makeStyles } from "@material-ui/core"
 import React from "react"
 
 import { colors } from "../styles/color"
-import { DOWNLOAD_LINK } from "./links"
+import { rootLevelBlock } from "../styles/layout"
+import { DownloadButton } from "./download-button"
+import { getDownloadLinks } from "./links"
 
 const useStyles = makeStyles((theme) =>
     createStyles({
         cta: {
+            width: "100%",
+            margin: "0 auto",
+
             marginTop: 120,
             marginBottom: 160,
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
+            background: theme.palette.primary.light,
+
+            paddingBottom: 40,
         },
         header: {
-            marginTop: 64,
-            marginBottom: 32,
+            marginTop: 0,
+            marginBottom: 0,
             fontWeight: 700,
-            color: colors.gray900,
+            color: theme.palette.common.white,
 
             fontSize: "2.2rem",
             [theme.breakpoints.up("sm")]: {
@@ -27,24 +35,41 @@ const useStyles = makeStyles((theme) =>
     }),
 )
 
-const CTAButton: React.FC<{ href: string }> = ({ href, children }) => {
-    return (
-        <Button disabled target="_blank" rel="noopener noreferrer" href={href} size="large" variant="contained" color="primary">
-            {children}
-        </Button>
-    )
-}
+// const DownloadButton: React.FC<{ href: string }> = ({ href, children }) => {
+//     const classes = useStyles()
+//     return (
+//         <Button
+//             target="_blank"
+//             rel="noopener noreferrer"
+//             href={href}
+//             size="large"
+//             // variant="contained"
+//             // color="primary"
+//             className={classes.downloadButton}
+//         >
+//             {children}
+//         </Button>
+//     )
+// }
 
 // call-to-action
 export const CTA: React.FC = () => {
     const classes = useStyles()
+    const version = "0.31.2"
+    const downloadLinks = getDownloadLinks(version)
 
     return (
         <div className={classes.cta}>
-            <h2 className={classes.header}>Get started with Rino</h2>
-            <Box display="flex">
-                <CTAButton href={DOWNLOAD_LINK}>comming soon</CTAButton>
-            </Box>
+            <h3 className={classes.header}>Download Rino today</h3>
+
+            <DownloadButton />
+
+            {/* <Box>v{version}</Box>
+            <Box className={classes.downloadButtonWrapper}>
+                <DownloadButton href={downloadLinks.mac}>Mac</DownloadButton>
+                <DownloadButton href={downloadLinks.win}>Windows</DownloadButton>
+                <DownloadButton href={downloadLinks.linux}>Linux</DownloadButton>
+            </Box> */}
         </div>
     )
 }
