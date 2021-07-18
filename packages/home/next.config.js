301c4f37969b4531c3e03347639aabdb57d049c3
ocavue <ocavue@gmail.com>
2021-07-19 00:22:09 +0800

    refactor(home): fetch the version number

--- packages/home/next.config.js
+++ packages/home/next.config.js
@@ -1,3 +1,15 @@
+const { readFileSync } = require("fs")
+const { join } = require("path")
 const getNextConfig = require("@rino.app/next/next.base.config")
 
-module.exports = getNextConfig()
+const baseConfig = getNextConfig()
+
+const version = readFileSync(join(__dirname, "..", "..", "version.txt"), "utf-8").trim()
+
+module.exports = {
+    ...baseConfig,
+    env: {
+        ...baseConfig.env,
+        NEXT_PUBLIC_ELECTRON_VERSION: version,
+    },
+}
