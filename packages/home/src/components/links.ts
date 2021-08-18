a931b003ca1af5a80af8eb6a2cf6da27bcbb2cc6
ocavue <ocavue@gmail.com>
2021-08-18 17:32:03 +0800

    chore: add discord link

--- packages/home/src/components/links.ts
+++ packages/home/src/components/links.ts
@@ -2,6 +2,7 @@ export const RELEASE_LINK = "https://github.com/ocavue/rino/releases"
 export const GITHUB_LINK = "https://github.com/ocavue/rino"
 export const MAILTO_LINK = "mailto:support@rino.app"
 export const TWITTER_LINK = "https://twitter.com/rino_editor"
+export const DISCORD_LINK = "https://rino.app/chat"
 
 /**
  * @param version a string like "0.31.2"
