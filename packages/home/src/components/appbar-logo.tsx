59482cca0e4626ce1848f786de75d26592944f2e
ocavue <ocavue@gmail.com>
2021-09-22 06:21:12 +0800

    fix

--- packages/home/src/components/appbar-logo.tsx
+++ packages/home/src/components/appbar-logo.tsx
@@ -1,59 +1,54 @@
-import { createStyles } from "@mui/styles"
-import { makeStyles } from "@mui/styles"
+import { Box } from "@mui/system"
 import React from "react"
 
 import { colors } from "../styles/color"
 import { Logo } from "./logo"
 
-const useStyles = makeStyles((theme) =>
-    createStyles({
-        root: {
-            display: "flex",
-            alignItems: "center",
-        },
-        logo: {
-            height: "48px",
-            width: "48px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            [theme.breakpoints.up("md")]: {
-                height: "64px",
-                width: "64px",
-            },
-        },
-        text: {
-            lineHeight: "1.5rem",
-            fontWeight: 500,
-            color: colors.gray700,
-            transitionProperty: "color",
-            transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
-            transitionDuration: "150ms",
-
-            fontSize: "1.25rem",
-            paddingLeft: "1rem",
-
-            "&:hover": {
-                color: colors.gray900,
-            },
-            "&:focus": {
-                outline: "0",
-                color: colors.gray900,
-            },
-            [theme.breakpoints.up("md")]: {
-                paddingLeft: "1.5rem",
-                fontSize: "1.5rem",
-            },
-        },
-    }),
-)
-
 export const AppbarLogo: React.FC = () => {
-    const classes = useStyles()
     return (
-        <div className={classes.root}>
-            <Logo className={classes.logo} />
-            <span className={classes.text}>Rino</span>
-        </div>
+        <Box
+            sx={{
+                display: "flex",
+                alignItems: "center",
+            }}
+        >
+            <Logo
+                sx={{
+                    height: { xs: "48px", md: "64px" },
+                    width: { xs: "48px", md: "64px" },
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            />
+            <Box
+                sx={{
+                    lineHeight: "1.5rem",
+                    fontWeight: 500,
+                    color: colors.gray700,
+                    transitionProperty: "color",
+                    transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+                    transitionDuration: "150ms",
+
+                    "&:hover": {
+                        color: colors.gray900,
+                    },
+                    "&:focus": {
+                        outline: "0",
+                        color: colors.gray900,
+                    },
+                    paddingLeft: {
+                        xs: "1rem",
+                        md: "1.5rem",
+                    },
+                    fontSize: {
+                        xs: "1.25rem",
+                        md: "1.5rem",
+                    },
+                }}
+            >
+                Rino
+            </Box>
+        </Box>
     )
 }
--- packages/home/src/components/logo.tsx
+++ packages/home/src/components/logo.tsx
@@ -1,8 +1,9 @@
+import { Box, SxProps } from "@mui/system"
 import React from "react"
 
 type HTMLImageProps = React.ComponentProps<"img">
-type LogoProps = Pick<HTMLImageProps, "className" | "style">
+type LogoProps = Pick<HTMLImageProps, "className" | "style"> & { sx?: SxProps }
 
 export const Logo: React.FC<LogoProps> = (props) => {
-    return <img src="/share/img/icons/safari-pinned-tab.svg" alt="Rino Logo" {...props} />
+    return <Box component="img" src="/share/img/icons/safari-pinned-tab.svg" alt="Rino Logo" {...props} />
 }
