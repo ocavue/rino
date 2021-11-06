4ae625ac15f38888182ac2ec5feb8f397b9e7f42
ocavue <ocavue@gmail.com>
2021-11-06 16:58:24 +0800

    use script

--- common/scripts/rush-cli.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# This sample script will be used in the CI environment, where Rush is not preinstalled.
-
-# Go to the root of the project
-cd $(dirname $0)/../..
-exec node "common/scripts/install-run-rush.js" "$@"
