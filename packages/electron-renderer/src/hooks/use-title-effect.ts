2543f3509dd08aab86cbabc9e58c72660e69f909
ocavue <ocavue@gmail.com>
2021-12-28 04:38:23 +0800

    remove log

--- packages/electron-renderer/src/hooks/use-title-effect.ts
+++ packages/electron-renderer/src/hooks/use-title-effect.ts
@@ -7,22 +7,12 @@ import type { WorkbenchState } from "./use-workbench"
 
 export function useTitleEffect(state: WorkbenchState): void {
     const title = useMemo(() => {
-        let title = basename(state.path)
-        if (!title) {
-            title = "Untitled"
-        }
+        let title = basename(state.path) || "Untitled"
         if (state.isSaving || state.isSerializing || state.hasUnsavedChanges) {
             title = `${title} - Edited`
         }
         return title
     }, [state.hasUnsavedChanges, state.isSaving, state.isSerializing, state.path])
-
-    console.log("useTitleEffect", title, {
-        content: !!state.content,
-        isSaving: !!state.isSaving,
-        isSerializing: !!state.isSerializing,
-        path: !!state.path,
-    })
 
     useEffect(() => {
         ipcInvoker.setTitle({ title })
