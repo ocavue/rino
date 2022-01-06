f9b7f1b590d770e032e745bf6e4eb8a23a983b7c
ocavue <ocavue@gmail.com>
2022-01-06 13:18:43 +0800

    fix(editor): correctly display images with paths containing whitespaces

---
+++ packages/editor/test/extensions/inline/format-href.spec.ts
@@ -0,0 +1,20 @@
+import { formatHref } from "../../../src/extensions/inline/format-href"
+
+const cases: Array<[string, string]> = [
+    ["/tmp/image.png", "file:///tmp/image.png"],
+    ["/tmp/UPPERCASE.IPEG", "file:///tmp/UPPERCASE.IPEG"],
+    ["/tmp/with whitespace.svg", "file:///tmp/with%20whitespace.svg"],
+    ["file:///tmp/with%20whitespace.svg", "file:///tmp/with%20whitespace.svg"],
+    ["https://example.com/hello.png", "https://example.com/hello.png"],
+    ["sftp://192.168.1.1/hello.png", "sftp://192.168.1.1/hello.png"],
+    [String.raw`C:\Documents\Newsletters\Summer2018.pdf`, "file:///C:/Documents/Newsletters/Summer2018.pdf"],
+    [String.raw`\Program Files\Custom Utilities\StringFinder.exe`, "file:///Program%20Files/Custom%20Utilities/StringFinder.exe"],
+]
+
+describe("formatHref", () => {
+    for (const [input, expected] of cases) {
+        test(input, () => {
+            expect(formatHref(input)).toEqual(expected)
+        })
+    }
+})
