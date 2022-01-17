84f4a48af84bf6958c4346d8951590d0b59ca0ab
ocavue <ocavue@gmail.com>
2022-01-17 17:17:23 +0800

    wip

--- packages/editor/src/extensions/inline/inline-mark-commands.ts
+++ packages/editor/src/extensions/inline/inline-mark-commands.ts
@@ -1,4 +1,4 @@
-import { CommandFunction, CommandFunctionProps } from "@remirror/core"
+import { CommandFunction, CommandFunctionProps, ProsemirrorNode } from "@remirror/core"
 import { TextSelection } from "prosemirror-state"
 
 import { updateRangeMarks } from "./inline-mark-helpers"
@@ -6,26 +6,78 @@ import { updateRangeMarks } from "./inline-mark-helpers"
 type CreateInlineKeyBindingProps = {
     left: string
     right: string
+    mark: ToggleableInlineMarkName
 }
 
-function doToggleInlineMark({ left, right }: CreateInlineKeyBindingProps): CommandFunction {
+function toggleSimpleInlineMark({ left, right, mark }: CreateInlineKeyBindingProps): CommandFunction {
     return (props: CommandFunctionProps): boolean => {
         const { tr, dispatch, state } = props
         const { $from, $to, anchor, head } = state.selection
 
-        if ($from.sameParent($to) && $from.parent.type.isTextblock) {
-            if (dispatch) {
-                tr.insertText(right, $to.pos)
-                tr.insertText(left, $from.pos)
-                const offset = left.length
-                tr.setSelection(TextSelection.create(tr.doc, anchor + offset, head + offset))
-                updateRangeMarks(tr)
-                dispatch(tr)
-            }
-            return true
+        if (!$from.sameParent($to) || !$from.parent.type.isTextblock) {
+            return false
         }
 
-        return false
+        const parent = $from.parent
+
+        let fromIndex = $from.index()
+        let toIndex = $to.index()
+
+        if (toIndex > fromIndex && $to.textOffset === 0) {
+            // `$to` is between two text nodes and `toIndex` is pointing to the right node. Let's make it point to the left node.
+            toIndex -= 1
+        }
+
+        if (fromIndex < toIndex) {
+            const toTextNode = parent.child(toIndex)
+            if (hasMark(toTextNode, "mdMark") && toTextNode.textContent === right) {
+                toIndex -= 1
+            }
+        }
+
+        if (fromIndex < toIndex) {
+            const fromTextNode = parent.child(fromIndex)
+            if (hasMark(fromTextNode, "mdMark") && fromTextNode.textContent === left) {
+                fromIndex += 1
+            }
+        }
+
+        if (fromIndex === toIndex) {
+            console.debug("same text node :)")
+            const textNode = parent.child(fromIndex)
+            const markType = state.schema.marks[mark]
+            if (!markType) {
+                console.warn(`failed to find mark type "${mark}" in the schema`)
+                return false
+            }
+
+            if (textNode.marks.map((mark) => mark.type.name).includes(mark)) {
+                // remove marks
+                const prevMarkIndex = fromIndex - 1
+                const nextMarkIndex = fromIndex + 1
+                if (dispatch && prevMarkIndex >= 0) {
+                    tr.delete()
+                }
+                return true
+            }
+        } else {
+            console.debug("different text node :(")
+        }
+
+        console.log("index", $from.index(), $to.index(), $from.indexAfter(), $to.indexAfter(), $from.parentOffset, $to.parentOffset)
+
+        const selectedSlice = parent.slice($from.parentOffset, $to.parentOffset)
+        console.log("selectedSlice", selectedSlice.content.childCount)
+
+        if (dispatch) {
+            tr.insertText(right, $to.pos)
+            tr.insertText(left, $from.pos)
+            const offset = left.length
+            tr.setSelection(TextSelection.create(tr.doc, anchor + offset, head + offset))
+            updateRangeMarks(tr)
+            dispatch(tr)
+        }
+        return true
     }
 }
 
@@ -34,13 +86,13 @@ export type ToggleableInlineMarkName = "mdStrong" | "mdEm" | "mdCodeText" | "mdD
 export function toggleInlineMark(mark: ToggleableInlineMarkName): CommandFunction {
     switch (mark) {
         case "mdStrong":
-            return doToggleInlineMark({ left: "**", right: "**" })
+            return toggleSimpleInlineMark({ mark, left: "**", right: "**" })
         case "mdEm":
-            return doToggleInlineMark({ left: "*", right: "*" })
+            return toggleSimpleInlineMark({ mark, left: "*", right: "*" })
         case "mdCodeText":
-            return doToggleInlineMark({ left: "`", right: "`" })
+            return toggleSimpleInlineMark({ mark, left: "`", right: "`" })
         case "mdDel":
-            return doToggleInlineMark({ left: "~~", right: "~~" })
+            return toggleSimpleInlineMark({ mark, left: "~~", right: "~~" })
         default:
             throwUnknownMarkError(mark)
     }
@@ -50,4 +102,8 @@ export type ToggleInlineMark = typeof toggleInlineMark
 
 function throwUnknownMarkError(mark: never): never {
     throw new Error(`Unknown mark to toggle: ${mark}`)
+}
+
+function hasMark(node: ProsemirrorNode, mark: string): boolean {
+    return node.marks.some((m) => m.type.name === mark)
 }
