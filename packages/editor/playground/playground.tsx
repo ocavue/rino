116a4c82f35258872d503dba18c9a415d3a1c4bb
asherascout <asherascout@gmail.com>
2022-02-09 09:21:48 +0800

    chore(editor): add a debug button on the playground page (#945)

--- packages/editor/playground/playground.tsx
+++ packages/editor/playground/playground.tsx
@@ -1,10 +1,11 @@
+import "./style.css"
+
 import { isString } from "lodash-es"
 import React, { FC, useMemo, useState } from "react"
 import ReactDOM from "react-dom"
 
 import type { WysiwygOptions } from "../src"
 import { Editor } from "../src"
-
 const singleRow = `hello **strong**! hello *italic*! hello \`code\`! hello [link](https://www.google.com)!`
 
 const longContent = (singleRow.repeat(200) + "\n\n").repeat(5)
@@ -45,6 +46,17 @@ const contentMap: { [key: string]: string } = {
     "just-code": justCodeContent,
     long: longContent,
     customize: "",
+}
+
+const DebugButton: FC<{ enableDevTools: boolean; toggleEnableDevTools: () => void }> = ({ enableDevTools, toggleEnableDevTools }) => {
+    return (
+        <button
+            className={enableDevTools ? "playground-debug-button-enable" : "playground-debug-button-disable"}
+            onClick={(e) => toggleEnableDevTools()}
+        >
+            Debug
+        </button>
+    )
 }
 
 /** focus this element to hide the cursor in the editor */
@@ -103,6 +115,8 @@ const DebugConsole: FC<{ hasUnsavedChanges: boolean; contentId: string; content:
                     fontSize: "85%",
                     borderRadius: "3px",
                     padding: "16px",
+                    overflowWrap: "break-word",
+                    overflow: "hidden",
                 }}
             >
                 {content}
@@ -119,7 +133,7 @@ const App: FC = () => {
     const params = new URLSearchParams(document.location.search)
     const initialContent = params.get("content")
     let initialContentId = params.get("contentid")
-    const enableDevTools = params.get("devtools") !== "false"
+    const defaultEnableDevTools = params.get("devtools") === "true"
 
     const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false)
 
@@ -135,6 +149,7 @@ const App: FC = () => {
 
     const [content, setContent] = useState(contentMap[defaultContentId])
     const [contentId, setContentId] = useState(defaultContentId)
+    const [enableDevTools, setEnableDevTools] = useState(defaultEnableDevTools)
 
     const note = useMemo(() => {
         return {
@@ -159,11 +174,12 @@ const App: FC = () => {
         />
     )
     return (
-        <div>
-            {enableDevTools ? (
-                <div style={{ display: "flex", flexDirection: "row" }}>
-                    <div style={{ width: "50%", height: "100%", overflow: "auto", position: "fixed" }}>{editor}</div>
-                    <div style={{ width: "50%", height: "100%", overflow: "auto", position: "fixed", right: "0" }}>
+        <div style={{ width: "100%" }}>
+            <DebugButton enableDevTools={enableDevTools} toggleEnableDevTools={() => setEnableDevTools(!enableDevTools)} />
+            <div className="playground-box">
+                <div className="playground-self-scroll">{editor}</div>
+                {enableDevTools ? (
+                    <div className="playground-self-scroll">
                         <DebugConsole
                             hasUnsavedChanges={hasUnsavedChanges}
                             contentId={contentId}
@@ -171,10 +187,8 @@ const App: FC = () => {
                             onSelect={changeContentSelect}
                         />
                     </div>
-                </div>
-            ) : (
-                <div style={{ display: "flex", flexDirection: "row" }}>{editor}</div>
-            )}
+                ) : null}
+            </div>
             <BlurHelper />
         </div>
     )
