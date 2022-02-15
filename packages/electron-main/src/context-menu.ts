47331cd60b6fb169fec261bb4dfd77c7077a20e0
ocavue <ocavue@gmail.com>
2022-02-15 21:40:21 +0800

    chore: import electron-context-menu

--- packages/electron-main/src/context-menu.ts
+++ packages/electron-main/src/context-menu.ts
@@ -1,6 +1,4 @@
-// Use "required" instead of "import" here because of https://github.com/sindresorhus/electron-context-menu/issues/148
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const contextMenu = require("electron-context-menu")
+import contextMenu from "electron-context-menu"
 
 export function setupContextMenu() {
     contextMenu({
