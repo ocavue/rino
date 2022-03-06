33b004c611b8c253c6b2ebdbcc18012568cefd95
ocavue <ocavue@users.noreply.github.com>
2022-03-06 19:01:45 +0800

    test(editor): use vitest to run unit tests (#972)

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,1 +1,62 @@
-export * from "./util"
+export function all(items: unknown[]) {
+    for (const item of items) {
+        if (!item) return false
+    }
+    return true
+}
+
+export function any(items: unknown[]) {
+    for (const item of items) {
+        if (item) return true
+    }
+    return false
+}
+
+export function generateRandomId(): string {
+    // https://stackoverflow.com/a/13403498
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
+
+/**
+ * Remove any common leading whitespace from every line in `text`.
+ * Inspired by Python's `textwrap.dedent`.
+ */
+export function dedent(text: string) {
+    let minWhitespace = -1
+    const lines = text.split("\n")
+    for (const line of lines) {
+        if (line.length > 0) {
+            const match = /^(\s*).*$/.exec(line)
+            if (match) {
+                minWhitespace = minWhitespace === -1 ? match[1].length : Math.min(minWhitespace, match[1].length)
+            } else {
+                return text
+            }
+        }
+    }
+    return lines.map((line) => (line.length > 0 ? line.slice(minWhitespace) : line)).join("\n")
+}
+
+export function isMac(): boolean {
+    // https://stackoverflow.com/a/27862868
+    return typeof navigator !== "undefined" ? navigator.platform.includes("Mac") : false
+}
+
+export const metaKey: "metaKey" | "ctrlKey" = isMac() ? "metaKey" : "ctrlKey"
+
+export function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+export function basename(filePath: string) {
+    return filePath.split("\\").pop()?.split("/").pop() ?? ""
+}
+
+async function sleepBeforeError(timeout: number): Promise<never> {
+    await sleep(timeout)
+    throw new Error(`timed out after ${timeout} milliseconds`)
+}
+
+export async function createTimeoutPromise<T>(promise: Promise<T>, delay: number): Promise<T> {
+    return await Promise.race([promise, sleepBeforeError(delay)])
+}
