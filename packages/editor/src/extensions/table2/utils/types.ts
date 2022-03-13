7f178999b21dba2078ab2e54344b0fd4edc978da
ocavue <ocavue@gmail.com>
2022-03-13 18:54:35 +0800

    Revert "Copy remirror table"
    This reverts commit 599e356516251be02c0c8202f66c80ec7030214d.

--- packages/editor/src/extensions/table2/utils/types.ts
+++
@@ -1,7 +0,0 @@
-import { FindProsemirrorNodeResult } from "@remirror/core"
-
-export type FindTable = () => FindProsemirrorNodeResult | undefined
-export interface CellAxis {
-    row: number
-    col: number
-}
