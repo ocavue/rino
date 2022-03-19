b7d9e7da42d26b283db90d33ec5b7d167aab476b
ocavue <ocavue@gmail.com>
2022-03-19 13:51:28 +0800

    basic style

--- packages/editor/src/extensions/table3/table-selector.ts
+++ packages/editor/src/extensions/table3/table-selector.ts
@@ -1,23 +1,61 @@
+import { css, injectGlobal } from "@emotion/css"
 import { EditorState, Selection } from "prosemirror-state"
 import { Decoration, DecorationSet } from "prosemirror-view"
 
 import { createElement as h, getCellsInColumn, getCellsInRect, getCellsInRow } from "./table-utils"
 
-function createTableSelector(selection: Selection): Decoration | null {
+injectGlobal`
+.remirror-editor.ProseMirror .tableWrapper {
+    overflow: visible;
+}
+
+.remirror-editor.ProseMirror table {
+    overflow: visible;
+}
+`
+
+const bodySelectorClass = css`
+    background: lightblue;
+    position: absolute;
+    width: 16px;
+    height: 16px;
+    top: -24px;
+    left: -24px;
+`
+
+const rowSelectorClass = css`
+    background: lightcoral;
+    position: absolute;
+    width: 16px;
+    top: 0;
+    bottom: 0;
+    left: -24px;
+`
+
+const columnSelectorClass = css`
+    background: lightgreen;
+    position: absolute;
+    height: 16px;
+    left: 0;
+    right: 0;
+    top: -24px;
+`
+
+function createBodySelector(selection: Selection): Decoration | null {
     const cell = getCellsInRect(selection, { top: 0, bottom: 1, left: 0, right: 1 }).at(0)
     if (!cell) {
         return null
     }
 
     return Decoration.widget(cell.pos + 1, (view, getPos) => {
-        return h("div", { style: "background: red;" }, "table selector")
+        return h("div", { class: bodySelectorClass, contenteditable: "false" })
     })
 }
 
 function createRowSelectors(selection: Selection): Decoration[] {
     return getCellsInColumn(selection, 0).map((cell, rowIndex) => {
         return Decoration.widget(cell.pos + 1, (view, getPos) => {
-            return h("div", { style: "background: green;" }, `row selector ${rowIndex}`)
+            return h("div", { class: rowSelectorClass, contenteditable: "false" })
         })
     })
 }
@@ -25,7 +63,7 @@ function createRowSelectors(selection: Selection): Decoration[] {
 function createColumnSelectors(selection: Selection): Decoration[] {
     return getCellsInRow(selection, 0).map((cell, columnIndex) => {
         return Decoration.widget(cell.pos + 1, (view, getPos) => {
-            return h("div", { style: "background: yellow;" }, `column selector ${columnIndex}`)
+            return h("div", { class: columnSelectorClass, contenteditable: "false" })
         })
     })
 }
@@ -33,7 +71,7 @@ function createColumnSelectors(selection: Selection): Decoration[] {
 export function createSelectors(state: EditorState): DecorationSet {
     const { doc, selection } = state
 
-    const tableSelector = createTableSelector(selection)
+    const tableSelector = createBodySelector(selection)
     if (!tableSelector) return DecorationSet.empty
 
     const rowSelectors = createRowSelectors(selection)
