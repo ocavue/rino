084d8765f50cbc90deec08cbd7bf0591c3149776
ocavue <ocavue@gmail.com>
2022-03-19 11:51:38 +0800

    disable resizable

--- packages/editor/src/components/wysiwyg/wysiwyg-extension.ts
+++ packages/editor/src/components/wysiwyg/wysiwyg-extension.ts
@@ -56,7 +56,7 @@ function createRinoMarkdownNodeExtensions() {
         new RinoBulletListExtension({}),
         new RinoOrderedListExtension(),
 
-        new RinoTableExtension(),
+        new RinoTableExtension({ resizable: false }),
         new RinoTableRowExtension(),
         new RinoTableCellExtension(),
         new RinoTableHeaderCellExtension(),
