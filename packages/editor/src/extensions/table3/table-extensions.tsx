44ae35addc04b2ad05b5ad43ddfa65c3f5b7c1f5
ocavue <ocavue@gmail.com>
2022-03-19 15:01:39 +0800

    add allowGapCursor

--- packages/editor/src/extensions/table3/table-extensions.tsx
+++ packages/editor/src/extensions/table3/table-extensions.tsx
@@ -1,8 +1,10 @@
+import { ApplySchemaAttributes, NodeSpecOverride } from "@remirror/core"
 import {
     TableCellExtension as BaseTableCellExtension,
     TableExtension as BaseTableExtension,
     TableHeaderCellExtension as BaseTableHeaderCellExtension,
     TableRowExtension as BaseTableRowExtension,
+    TableSchemaSpec,
 } from "@remirror/extension-tables"
 import { EditorState } from "prosemirror-state"
 import { DecorationSet } from "prosemirror-view"
@@ -23,16 +25,28 @@ export class TableCellExtension extends BaseTableCellExtension {
     get name() {
         return "tableCell" as const
     }
+
+    createNodeSpec(extra: ApplySchemaAttributes, override: NodeSpecOverride): TableSchemaSpec {
+        return { ...super.createNodeSpec(extra, override), allowGapCursor: false }
+    }
 }
 
 export class TableHeaderCellExtension extends BaseTableHeaderCellExtension {
     get name() {
         return "tableHeaderCell" as const
     }
+
+    createNodeSpec(extra: ApplySchemaAttributes, override: NodeSpecOverride): TableSchemaSpec {
+        return { ...super.createNodeSpec(extra, override), allowGapCursor: false }
+    }
 }
 
 export class TableRowExtension extends BaseTableRowExtension {
     get name() {
         return "tableRow" as const
+    }
+
+    createNodeSpec(extra: ApplySchemaAttributes, override: NodeSpecOverride): TableSchemaSpec {
+        return { ...super.createNodeSpec(extra, override), allowGapCursor: false }
     }
 }
