2120ffe04161032619314efb6420622c9aed6257
ocavue <ocavue@gmail.com>
2022-03-19 17:07:21 +0800

    refactor operations

--- packages/editor/src/extensions/table3/table-operation.ts
+++ packages/editor/src/extensions/table3/table-operation.ts
@@ -2,61 +2,50 @@ import { findParentNodeOfType } from "@remirror/core"
 import { CellSelection, TableMap } from "prosemirror-tables"
 import { EditorView } from "prosemirror-view"
 
-function validateTable(view: EditorView, pos: number) {
-    const $pos = view.state.doc.resolve(pos)
-    const table = findParentNodeOfType({ selection: $pos, types: "table" })
-    const cell = findParentNodeOfType({ selection: $pos, types: "tableCell" })
-
-    if (!table || !cell) {
-        return null
-    }
-
-    const map = TableMap.get(table.node)
-    if (map.map.length === 0) {
-        return null
-    }
-
-    return { map, tableStart: table.start, cellPos: cell.pos }
-}
-
 export function selectRow(view: EditorView, pos: number) {
-    const details = validateTable(view, pos)
+    const cell = findParentNodeOfType({ selection: view.state.doc.resolve(pos), types: "tableCell" })
 
-    if (!details) {
+    if (!cell) {
         return
     }
 
     const tr = view.state.tr
-    const selection = CellSelection.rowSelection(tr.doc.resolve(details.cellPos))
+    const selection = CellSelection.rowSelection(tr.doc.resolve(cell.pos))
     // @ts-expect-error CellSelection has incorrect type
     view.dispatch(tr.setSelection(selection))
 }
 
 export function selectColumn(view: EditorView, pos: number) {
-    const details = validateTable(view, pos)
+    const cell = findParentNodeOfType({ selection: view.state.doc.resolve(pos), types: "tableCell" })
 
-    if (!details) {
+    if (!cell) {
         return
     }
 
     const tr = view.state.tr
-    const selection = CellSelection.colSelection(tr.doc.resolve(details.cellPos))
+    const selection = CellSelection.colSelection(tr.doc.resolve(cell.pos))
     // @ts-expect-error CellSelection has incorrect type
     view.dispatch(tr.setSelection(selection))
 }
 
 export function selectTable(view: EditorView, pos: number) {
-    const details = validateTable(view, pos)
+    const table = findParentNodeOfType({ selection: view.state.doc.resolve(pos), types: "table" })
 
-    if (!details) {
+    if (!table) {
         return
     }
 
-    const { map, tableStart } = details
+    const map = TableMap.get(table.node)
+
+    if (!map.map.length) {
+        return
+    }
+
+    const firstCellPos = map.map[0]
+    const lastCellPos = map.map[map.map.length - 1]
+
     const tr = view.state.tr
-    const firstCellPosInTable = map.map[0]
-    const lastCellPosInTable = map.map[map.map.length - 1]
-    const selection = CellSelection.create(tr.doc, tableStart + firstCellPosInTable, tableStart + lastCellPosInTable)
+    const selection = CellSelection.create(tr.doc, table.start + firstCellPos, table.start + lastCellPos)
     // @ts-expect-error CellSelection has incorrect type
     view.dispatch(tr.setSelection(selection))
 }
