cce64589134dd8529a896d0bc3f7611c32507a1c
ocavue <ocavue@gmail.com>
2022-03-19 15:24:30 +0800

    move css

--- packages/editor/src/extensions/table3/table-selector.ts
+++ packages/editor/src/extensions/table3/table-selector.ts
@@ -1,10 +1,76 @@
+import { injectGlobal as css } from "@emotion/css"
 import { EditorState, Selection } from "prosemirror-state"
 import { Decoration, DecorationSet } from "prosemirror-view"
 
-import { injectGlobalStyles } from "./table-selector-style"
 import { createElement as h, getCellsInColumn, getCellsInRect, getCellsInRow } from "./table-utils"
 
-injectGlobalStyles()
+css`
+    .remirror-editor.ProseMirror .tableWrapper {
+        overflow: visible;
+    }
+
+    .remirror-editor.ProseMirror table {
+        overflow: visible;
+
+        .remirror-table-selector {
+            cursor: pointer;
+
+            background-color: #e5e5e5;
+            transition: background-color 0.15s ease-in-out;
+
+            &:hover {
+                transition: background-color 0.05s ease-in-out;
+                background-color: #bbbbbb;
+            }
+        }
+
+        .remirror-table-body-selector {
+            position: absolute;
+            width: 16px;
+            height: 16px;
+            top: -22px;
+            left: -22px;
+        }
+
+        /* First implementation  */
+        /* .remirror-table-row-selector {
+                position: absolute;
+                top: 0;
+                bottom: 0;
+                left: 0;
+                width: 0;
+
+                &::after {
+                    background-color: red;
+                    cursor: pointer;
+                    position: absolute;
+                    width: 16px;
+                    top: 0;
+                    bottom: 0;
+                    left: -24px;
+                    display: block;
+                    content: "";
+                }
+            } */
+
+        .remirror-table-row-selector {
+            position: absolute;
+            width: 16px;
+            top: 0;
+            bottom: 0;
+            left: -22px;
+        }
+
+        /* Second implementation */
+        .remirror-table-column-selector {
+            position: absolute;
+            height: 16px;
+            left: 0;
+            right: 0;
+            top: -22px;
+        }
+    }
+`
 
 function createBodySelector(selection: Selection): Decoration | null {
     const cell = getCellsInRect(selection, { top: 0, bottom: 1, left: 0, right: 1 }).at(0)
