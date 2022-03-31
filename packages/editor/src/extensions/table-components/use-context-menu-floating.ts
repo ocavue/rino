c2cc6992dedbe150ca73af0f9decf1aac58d32e9
ocavue <ocavue@gmail.com>
2022-03-31 18:10:00 +0800

    rm useContextMenuFloating

--- packages/editor/src/extensions/table-components/use-context-menu-floating.ts
+++ packages/editor/src/extensions/table-components/use-context-menu-floating.ts
@@ -1,6 +1,6 @@
 import type { VirtualElement } from "@floating-ui/dom"
 import { shift, useFloating } from "@floating-ui/react-dom"
-import React, { useCallback, useEffect } from "react"
+import React, { useEffect } from "react"
 
 import { useOnClickOutside } from "./use-on-click-outside"
 
@@ -9,58 +9,6 @@ export type UseFloatingReturn = ReturnType<typeof useFloating>
 export type UseContextMenuFloatingReturn = UseFloatingReturn & {
     show: boolean
     clickHandler: (event: MouseEvent | React.MouseEvent) => void
-}
-
-export function useContextMenuFloating(): UseContextMenuFloatingReturn {
-    const [show, setShowMenu] = React.useState(false)
-
-    const closeMenu = useCallback(() => {
-        setShowMenu(false)
-    }, [])
-
-    const useFloatingReturn = useFloating({
-        placement: "right-start",
-        middleware: [
-            shift({
-                mainAxis: true,
-                crossAxis: true,
-            }),
-        ],
-    })
-
-    const { reference, refs } = useFloatingReturn
-
-    useOnClickOutside(refs.floating, closeMenu)
-
-    const clickHandler = useCallback(
-        (event: MouseEvent | React.MouseEvent) => {
-            const selector = event.target as HTMLElement
-            console.log("selector:", selector.getBoundingClientRect())
-
-            setShowMenu(true)
-
-            const { clientX, clientY } = event
-            const virtualEl = {
-                getBoundingClientRect() {
-                    return {
-                        width: 0,
-                        height: 0,
-                        x: clientX,
-                        y: clientY,
-                        top: clientY,
-                        left: clientX,
-                        right: clientX,
-                        bottom: clientY,
-                    }
-                },
-                contextElement: selector,
-            }
-            reference(virtualEl)
-        },
-        [reference],
-    )
-
-    return { ...useFloatingReturn, show, clickHandler }
 }
 
 export type useContextMenuFloatingV2Props = {
