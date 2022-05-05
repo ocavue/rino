e2e2dbfb0beffb7cc9b3325e475798419c5374a5
ocavue <ocavue@gmail.com>
2022-05-06 00:17:12 +0800

    update scripts/generate-lighthouserc.js

--- scripts/generate-lighthouserc.js
+++ scripts/generate-lighthouserc.js
@@ -11,41 +11,8 @@ const commonAssertions = {
     // Since the website is not deployed on my own server, I can't do much about the HTTP response headers.
     "csp-xss": "warn",
 
-    // `react-dom/cjs/react-dom.production.min.js` will be marked as a `unused-javascript`. I can do nothing about it.
-    "unused-javascript": "warn",
-}
-
-const webConfig = {
-    ci: {
-        collect: {
-            startServerCommand: "echo 'nothing to do'",
-            url: [
-                `https://rino-web-${slugifyBranch}.ocavue.vercel.app/sign-in`,
-                `https://rino-web-${slugifyBranch}.ocavue.vercel.app/sign-up`,
-                `https://rino-web-${slugifyBranch}.ocavue.vercel.app/password-reset`,
-            ],
-        },
-        upload: {
-            target: "temporary-public-storage",
-        },
-        assert: {
-            preset: "lighthouse:recommended",
-            assertions: {
-                ...commonAssertions,
-
-                // Currently there are 9 javascript bundle files who has a large part of unused code. Firebase contributes most part of them.
-                // Check this link for the method to get all unused javascript code: https://web.dev/remove-unused-code/
-                // Check this link to follow the progress of tree-shakeable Firebase library: https://github.com/firebase/firebase-js-sdk/issues/2241
-                "unused-javascript": ["warn"],
-
-                // `@firebase/firestore/dict/index.cjs.js` has `unload` listeners.
-                // https://developers.google.com/web/updates/2018/07/page-lifecycle-api#the-unload-event
-                "no-unload-listeners": "warn",
-
-                "non-composited-animations": "warn",
-            },
-        },
-    },
+    // Haven't found time to fix this.
+    "non-composited-animations": "warn",
 }
 
 const homeConfig = {
@@ -66,5 +33,4 @@ const homeConfig = {
     },
 }
 
-fs.writeFileSync(path.join(__dirname, "..", ".lighthouserc.web.json"), JSON.stringify(webConfig))
 fs.writeFileSync(path.join(__dirname, "..", ".lighthouserc.home.json"), JSON.stringify(homeConfig))
