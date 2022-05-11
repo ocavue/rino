e6bacd822aad0d6541147cfe9f6fd50b16ad06df
ocavue <ocavue@gmail.com>
2022-05-11 13:38:42 +0800

    chore: fix coverage upload in CI

--- scripts/test-coverge.sh
+++ scripts/test-coverge.sh
@@ -13,8 +13,13 @@ merge-istanbul --out ./coverage/e2e/coverage-e2e.json './packages/*/coverage_e2e
 merge-istanbul --out ./coverage/uni/coverage-uni.json './packages/*/coverage/coverage-final.json'
 merge-istanbul --out ./coverage/all/coverage-all.json ./coverage/e2e/coverage-e2e.json ./coverage/uni/coverage-uni.json
 
+cp ./coverage/all/coverage-all.json ./coverage/coverage-final.json
+
 nyc report -t ./coverage/e2e --report-dir ./coverage/e2e --reporter=html --reporter=text-summary
 nyc report -t ./coverage/uni --report-dir ./coverage/uni --reporter=html --reporter=text-summary
 nyc report -t ./coverage/all --report-dir ./coverage/all --reporter=html --reporter=text-summary
 
-echo "Coverage report is in $PWD/coverage/all/index.html"
+
+echo "Coverage report:"
+echo "$PWD/coverage/coverage-final.json"
+echo "$PWD/coverage/all/index.html"
