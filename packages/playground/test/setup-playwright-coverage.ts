efb680f89bb447a3df8a4a2730be3996beb87d9c
ocavue <ocavue@gmail.com>
2022-05-30 22:13:28 +0800

    fix playwright

--- packages/playground/test/setup-playwright-coverage.ts
+++ packages/playground/test/setup-playwright-coverage.ts
@@ -1,11 +1,27 @@
-import { afterAll } from "vitest"
+import { afterAll, beforeAll } from "vitest"
 
 import { collectJSCoverage, startJSCoverage } from "./coverage"
 
-beforeAll(async () => {
+async function setupBrowser() {
+    const Playwright = await import("playwright-chromium")
+    const browser = await Playwright.chromium.launch({
+        headless: !!process.env.CI,
+        executablePath: process.env.PLAYWRIGHT_CHROME_EXECUTABLE_PATH,
+    })
+    const page = await browser.newPage()
+
+    globalThis.browser = browser
+    globalThis.page = page
+}
+
+if (!globalThis.browser) {
+    await setupBrowser()
+}
+
+beforeAll(async (ctx) => {
     await startJSCoverage()
 })
 
-afterAll(async () => {
+afterAll(async (ctx) => {
     await collectJSCoverage()
 })
