8f6ed176e59bb51a6f18118c7330dac13bc2cb81
ocavue <ocavue@users.noreply.github.com>
2022-06-05 19:45:23 +0800

    feat(editor): add a label before a heading (#1048)

--- packages/playground/src/content.ts
+++ packages/playground/src/content.ts
@@ -60,10 +60,25 @@ const tableContent = `
 
 `
 
+const headingContent = `
+# Heading Level 1
+
+## Heading Level 2
+
+### Heading Level 3
+
+#### Heading Level 4
+
+##### Heading Level 5
+
+###### Heading Level 6
+`
+
 export const contentMap: { [key: string]: string } = {
     default: defaultContent,
     "just-code": justCodeContent,
     long: longContent,
     table: tableContent,
+    heading: headingContent,
     customize: "",
 }
