3fa9e315e37fc2d3929d8e5b00f16382b075b521
ocavue <ocavue@gmail.com>
2022-06-08 22:39:25 +0800

    chore(ci): build electron in parallel

--- packages/cli/src/commands/build.mjs
+++ packages/cli/src/commands/build.mjs
@@ -4,7 +4,7 @@ import { findTurboBin } from "../utils/find-turbo-bin.mjs"
 
 export function build(packages, { onlyDependencys = false } = {}) {
     const turboBin = findTurboBin()
-    const args = ["run", "build", ...packages.map((name) => `--filter=${name}${onlyDependencys ? "^" : ""}...`)]
+    const args = ["run", "build", ...packages.map((name) => `--filter=${name}${onlyDependencys ? "^..." : ""}`)]
     console.log("$", turboBin + " " + args.join(" "))
     spawnSync(turboBin, args, { stdio: "inherit" })
 }
