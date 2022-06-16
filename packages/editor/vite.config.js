d9b931dae1e4c432cdc0e1315c4b56ee281ae25b
ocavue <ocavue@users.noreply.github.com>
2022-06-16 08:14:31 +0000

    try to install case-anything v2

--- packages/editor/vite.config.js
+++ packages/editor/vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig((env) => ({
     test: {
-        environment: "jsdom",
+        // environment: "jsdom",
         globals: true,
         setupFiles: ["./test/setup-vitest.ts"],
         coverage: {
@@ -16,7 +16,7 @@ export default defineConfig((env) => ({
             exclude: ["**/*.spec.*"],
         },
         deps: {
-            inline: ["@lingui/detect-locale"],
+            inline: [/lingui/],
         },
     },
 }))
