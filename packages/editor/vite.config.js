c563efea0ad9353ae385891774c86c92b50d58cc
ocavue <ocavue@gmail.com>
2022-06-19 21:50:50 +0800

    0.0.0-pr1713.6

--- packages/editor/vite.config.js
+++ packages/editor/vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig((env) => ({
     test: {
-        // environment: "jsdom",
+        environment: "jsdom",
         globals: true,
         setupFiles: ["./test/setup-vitest.ts"],
         coverage: {
