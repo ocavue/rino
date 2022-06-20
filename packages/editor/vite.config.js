f1d8b688a5798894f61846b46a8e5447a63a57e6
ocavue <ocavue@gmail.com>
2022-06-20 12:18:20 +0800

    Try maxConcurrency

--- packages/editor/vite.config.js
+++ packages/editor/vite.config.js
@@ -18,5 +18,8 @@ export default defineConfig((env) => ({
         deps: {
             inline: [/lingui/],
         },
+
+        // Limit the resources we used in the CI, to avoid out-of-memory errors.
+        maxConcurrency: process.env.CI ? 1 : 5,
     },
 }))
