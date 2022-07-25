171fa85801f2582651f370c82f1351e20d86cc51
ocavue <ocavue@gmail.com>
2022-07-25 21:22:21 +0800

    more tests

--- packages/editor/src/extensions/list-item/list-item-keymap.spec.ts
+++ packages/editor/src/extensions/list-item/list-item-keymap.spec.ts
@@ -33,161 +33,188 @@ describe("Enter", () => {
 
     let editor: ReturnType<typeof add>
 
-    describe("flat", () => {
-        it("can split non empty item", () => {
-            editor = add(
-                doc(
+    it("can split non-empty item", () => {
+        editor = add(
+            doc(
+                //
+                oli(p("123")),
+                oli(p("456<cursor>")),
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                //
+                oli(p("123")),
+                oli(p("456")),
+                oli(p("<cursor>")),
+            ),
+        )
+
+        editor = add(
+            doc(
+                //
+                oli(p("123")),
+                oli(p("45<cursor>6")),
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                //
+                oli(p("123")),
+                oli(p("45")),
+                oli(p("<cursor>6")),
+            ),
+        )
+
+        editor = add(
+            doc(
+                //
+                oli(p("1<cursor>23")),
+                oli(p("456")),
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                //
+                oli(p("1")),
+                oli(p("<cursor>23")),
+                oli(p("456")),
+            ),
+        )
+    })
+
+    it("can split non-empty sub item", () => {
+        editor = add(
+            doc(
+                oli(
                     //
-                    oli(p("123")),
+                    p("123"),
                     oli(p("456<cursor>")),
                 ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                oli(
                     //
-                    oli(p("123")),
+                    p("123"),
                     oli(p("456")),
                     oli(p("<cursor>")),
                 ),
-            )
-
-            editor = add(
-                doc(
-                    //
-                    oli(p("123")),
-                    oli(p("45<cursor>6")),
-                ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
-                    //
-                    oli(p("123")),
-                    oli(p("45")),
-                    oli(p("<cursor>6")),
-                ),
-            )
-
-            editor = add(
-                doc(
-                    //
-                    oli(p("1<cursor>23")),
-                    oli(p("456")),
-                ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
-                    //
-                    oli(p("1")),
-                    oli(p("<cursor>23")),
-                    oli(p("456")),
-                ),
-            )
-        })
-
-        it("can delete empty item", () => {
-            editor = add(
-                doc(
-                    //
-                    oli(p("123")),
-                    oli(p("<cursor>")),
-                ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
-                    //
-                    oli(p("123")),
-                    p("<cursor>"),
-                ),
-            )
-
-            editor = add(
-                doc(
-                    //
-                    oli(p("123")),
-                    oli(p("<cursor>")),
-                    oli(p("456")),
-                ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
-                    //
-                    oli(p("123")),
-                    p("<cursor>"),
-                    oli(p("456")),
-                ),
-            )
-
-            editor = add(
-                doc(
-                    //
-                    oli(p("<cursor>")),
-                    oli(p("123")),
-                ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
-                    //
-                    p("<cursor>"),
-                    oli(p("123")),
-                ),
-            )
-        })
+            ),
+        )
     })
 
-    describe("nested", () => {
-        it("can split non-empty sub item", () => {
-            editor = add(
-                doc(
-                    oli(
-                        p("123"),
-                        //
-                        oli(p("456<cursor>")),
-                    ),
-                ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
-                    oli(
-                        p("123"),
-                        //
-                        oli(p("456")),
-                        oli(p("<cursor>")),
-                    ),
-                ),
-            )
-        })
+    it("can delete empty item", () => {
+        editor = add(
+            doc(
+                //
+                oli(p("123")),
+                oli(p("<cursor>")),
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                //
+                oli(p("123")),
+                p("<cursor>"),
+            ),
+        )
 
-        it("can dedent empty sub item", () => {
-            editor = add(
-                doc(
-                    oli(
-                        p("123"),
-                        //
-                        oli(p("<cursor>")),
-                    ),
-                ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
-                    //
-                    oli(p("123"), p("<cursor>")),
-                ),
-            )
-        })
+        editor = add(
+            doc(
+                //
+                oli(p("123")),
+                oli(p("<cursor>")),
+                oli(p("456")),
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                //
+                oli(p("123")),
+                p("<cursor>"),
+                oli(p("456")),
+            ),
+        )
+
+        editor = add(
+            doc(
+                //
+                oli(p("<cursor>")),
+                oli(p("123")),
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                //
+                p("<cursor>"),
+                oli(p("123")),
+            ),
+        )
     })
 
-    describe("multiple paragraphs", () => {
-        it("escapes the item when the cursor is in the first paragraph of the item", () => {
-            editor = add(
-                doc(
+    it("can dedent empty sub item", () => {
+        editor = add(
+            doc(
+                oli(
+                    //
+                    p("123"),
+                    oli(p("<cursor>")),
+                ),
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                oli(
+                    //
+                    p("123"),
+                    p("<cursor>"),
+                ),
+            ),
+        )
+    })
+
+    it("escapes the item when the cursor is in the first paragraph of the item", () => {
+        editor = add(
+            doc(
+                oli(
+                    //
+                    p("123<cursor>"),
+                    p("456"),
+                    p("789"),
+                ),
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                oli(
+                    //
+                    p("123"),
+                ),
+                oli(
+                    //
+                    p("<cursor>"),
+                    p("456"),
+                    p("789"),
+                ),
+            ),
+        )
+
+        // Nested list item
+        editor = add(
+            doc(
+                oli(
+                    p("0"),
                     oli(
                         //
                         p("123<cursor>"),
@@ -195,10 +222,13 @@ describe("Enter", () => {
                         p("789"),
                     ),
                 ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                oli(
+                    p("0"),
                     oli(
                         //
                         p("123"),
@@ -210,56 +240,100 @@ describe("Enter", () => {
                         p("789"),
                     ),
                 ),
-            )
+            ),
+        )
+    })
 
-            // Nested list item
-            editor = add(
-                doc(
-                    oli(
-                        p("0"),
-                        oli(
-                            //
-                            p("123<cursor>"),
-                            p("456"),
-                            p("789"),
-                        ),
-                    ),
+    it("does not escapes the item when the cursor is not in the first paragraph of the item", () => {
+        // Cursor in the last paragraph of the item
+        editor = add(
+            doc(
+                oli(
+                    //
+                    p("123"),
+                    p("456<cursor>"),
                 ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
-                    oli(
-                        p("0"),
-                        oli(
-                            //
-                            p("123"),
-                        ),
-                        oli(
-                            //
-                            p("<cursor>"),
-                            p("456"),
-                            p("789"),
-                        ),
-                    ),
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                oli(
+                    //
+                    p("123"),
+                    p("456"),
+                    p("<cursor>"),
                 ),
-            )
-        })
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                oli(
+                    //
+                    p("123"),
+                    p("456"),
+                    p(""),
+                    p("<cursor>"),
+                ),
+            ),
+        )
 
-        it("does not escapes the item when the cursor is not in the first paragraph of the item", () => {
-            // Cursor in the last paragraph of the item
-            editor = add(
-                doc(
+        // Cursor in the middle paragraph of the item
+        editor = add(
+            doc(
+                oli(
+                    //
+                    p("123"),
+                    p("456<cursor>"),
+                    p("789"),
+                ),
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                oli(
+                    //
+                    p("123"),
+                    p("456"),
+                    p("<cursor>"),
+                    p("789"),
+                ),
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                oli(
+                    //
+                    p("123"),
+                    p("456"),
+                    p(""),
+                    p("<cursor>"),
+                    p("789"),
+                ),
+            ),
+        )
+
+        // Cursor in the last paragraph of the item (nested list item)
+        editor = add(
+            doc(
+                oli(
+                    p(),
                     oli(
                         //
                         p("123"),
                         p("456<cursor>"),
                     ),
                 ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                oli(
+                    p(),
                     oli(
                         //
                         p("123"),
@@ -267,10 +341,13 @@ describe("Enter", () => {
                         p("<cursor>"),
                     ),
                 ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                oli(
+                    p(),
                     oli(
                         //
                         p("123"),
@@ -279,11 +356,14 @@ describe("Enter", () => {
                         p("<cursor>"),
                     ),
                 ),
-            )
+            ),
+        )
 
-            // Cursor in the middle paragraph of the item
-            editor = add(
-                doc(
+        // Cursor in the middle paragraph of the item (nested list item)
+        editor = add(
+            doc(
+                oli(
+                    p(),
                     oli(
                         //
                         p("123"),
@@ -291,10 +371,13 @@ describe("Enter", () => {
                         p("789"),
                     ),
                 ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                oli(
+                    p(),
                     oli(
                         //
                         p("123"),
@@ -303,10 +386,13 @@ describe("Enter", () => {
                         p("789"),
                     ),
                 ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
+            ),
+        )
+        editor.press("Enter")
+        expect(editor.state).toEqualRemirrorState(
+            doc(
+                oli(
+                    p(),
                     oli(
                         //
                         p("123"),
@@ -316,97 +402,8 @@ describe("Enter", () => {
                         p("789"),
                     ),
                 ),
-            )
-
-            // Cursor in the last paragraph of the item (nested list item)
-            editor = add(
-                doc(
-                    oli(
-                        p(),
-                        oli(
-                            //
-                            p("123"),
-                            p("456<cursor>"),
-                        ),
-                    ),
-                ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
-                    oli(
-                        p(),
-                        oli(
-                            //
-                            p("123"),
-                            p("456"),
-                            p("<cursor>"),
-                        ),
-                    ),
-                ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
-                    oli(
-                        p(),
-                        oli(
-                            //
-                            p("123"),
-                            p("456"),
-                            p(""),
-                            p("<cursor>"),
-                        ),
-                    ),
-                ),
-            )
-
-            // Cursor in the middle paragraph of the item (nested list item)
-            editor = add(
-                doc(
-                    oli(
-                        p(),
-                        oli(
-                            //
-                            p("123"),
-                            p("456<cursor>"),
-                            p("789"),
-                        ),
-                    ),
-                ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
-                    oli(
-                        p(),
-                        oli(
-                            //
-                            p("123"),
-                            p("456"),
-                            p("<cursor>"),
-                            p("789"),
-                        ),
-                    ),
-                ),
-            )
-            editor.press("Enter")
-            expect(editor.state).toEqualRemirrorState(
-                doc(
-                    oli(
-                        p(),
-                        oli(
-                            //
-                            p("123"),
-                            p("456"),
-                            p(""),
-                            p("<cursor>"),
-                            p("789"),
-                        ),
-                    ),
-                ),
-            )
-        })
+            ),
+        )
     })
 
     describe("extra cases", () => {
