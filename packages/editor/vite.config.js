554d8335e5532a1eb5596a4a2910fc7fb6758715
ocavue <ocavue@users.noreply.github.com>
2022-11-24 01:45:47 +0800

    chore: update eslint config (#1165)

--- packages/editor/vite.config.js
+++ packages/editor/vite.config.js
@@ -4,7 +4,7 @@
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
-export default defineConfig((env) => ({
+export default defineConfig(() => ({
     test: {
         environment: "jsdom",
         globals: true,
