4e77b843870f7518ee301239c432f4fbb2cb4207
ocavue <ocavue@users.noreply.github.com>
2023-01-15 15:46:12 +0800

    chore(deps): update dependencies (#1186)

--- packages/editor/esbuild.mjs
+++ packages/editor/esbuild.mjs
@@ -1,42 +1,31 @@
 // @ts-check
 
-import { build } from "esbuild"
+import * as esbuild from "esbuild"
 import { nodeExternalsPlugin } from "esbuild-node-externals"
 
 /**
  * @param {import('esbuild').BuildOptions} options
  */
 async function main(options) {
+    const context = await esbuild.context(options)
     const watch = process.argv.slice(2).includes("--watch")
 
-    await build({
-        watch: watch
-            ? {
-                  onRebuild(error, result) {
-                      const date = new Date().toISOString().split("T")[1].split("Z")[0]
-                      if (error) {
-                          console.log(`[esbuild] ${date} error found`)
-                      } else {
-                          console.log(`[esbuild] ${date} watch build succeeded`)
-                      }
-                  },
-              }
-            : false,
-        plugins: [nodeExternalsPlugin()],
-        splitting: true,
-        ...options,
-    })
-
     if (watch) {
-        console.log("[esbuild] watching...")
+        context.watch()
+    } else {
+        await context.rebuild()
+        await context.dispose()
     }
 }
 
 main({
+    plugins: [nodeExternalsPlugin()],
+    splitting: true,
     entryPoints: { "rino-editor": "./src/index.ts" },
     outExtension: { ".js": ".mjs" },
     outdir: "./dist/",
     bundle: true,
     format: "esm",
     sourcemap: true,
+    logLevel: "debug",
 })
