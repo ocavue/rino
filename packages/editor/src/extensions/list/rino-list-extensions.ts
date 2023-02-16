778acef32233e778becf192746ef77f565504876
ocavue <ocavue@gmail.com>
2023-02-16 21:10:02 +0800

    fix: parse checkbox

--- packages/editor/src/extensions/list/rino-list-extensions.ts
+++ packages/editor/src/extensions/list/rino-list-extensions.ts
@@ -1,3 +1,4 @@
+import { isString } from "@remirror/core"
 import type Token from "markdown-it/lib/token"
 import { ListAttributes, ListExtension } from "remirror-extension-flat-list"
 import type { MarkdownParseState, NodeSerializerOptions } from "../../transform"
@@ -16,10 +17,24 @@ export class RinoListExtension extends ListExtension implements MarkdownNodeExte
                             state.openNode(this.type, { type: "ordered" } satisfies ListAttributes)
                             break
                         case "bullet_list":
-                            state.openNode(this.type)
+                            state.openNode(this.type, { type: "bullet" } satisfies ListAttributes)
                             break
                         default:
                             throw new Error("unknown context")
+                    }
+                },
+            },
+            {
+                type: ParserRuleType.free,
+                token: "list_checkbox",
+                handler: (state: MarkdownParseState, tok: Token) => {
+                    const parent = state.stack[state.stack.length - 1]
+                    if (parent?.type.name === "list") {
+                        const checked: null | string | boolean = tok.attrGet("checked")
+                        const attrs: ListAttributes = { type: "task", checked: isString(checked) || !!checked }
+                        parent.attrs = attrs
+                    } else {
+                        console.warn(`expect list but got ${parent?.type.name}`)
                     }
                 },
             },
