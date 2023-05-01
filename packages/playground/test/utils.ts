ab789e618b1ab0f8b06d4a70d8e10bbb043455ba
ocavue <ocavue@gmail.com>
2023-05-01 15:09:40 +0800

    Revert "fix test"
    This reverts commit 7d6cab90a6ebe44577f08af9ef8da4252f1cf554.

--- packages/playground/test/utils.ts
+++ packages/playground/test/utils.ts
@@ -137,10 +137,9 @@ export const wysiwygEditorSelector = testidSelector("wysiwyg_mode_textarea")
 export const sourceCodeEditorSelector = testidSelector("source_code_mode_textarea")
 
 export async function getSourceCodeModeText(): Promise<string> {
-    const text = await page.$eval("html", () => {
+    return await page.$eval("html", () => {
         return (window as any)._RINO_EDITOR_VIEW.state.doc.textContent as string
     })
-    return text.replace(/\<\!-\-\s*\-\-\>$/, "").trim()
 }
 
 export async function waitAnimation<T>(promise: Promise<T>, ms = 500): Promise<T> {
